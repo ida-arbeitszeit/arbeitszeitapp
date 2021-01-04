225b535553a8d260ce9a6e85b26b693be0fe4e49
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-05 00:25:47 +0100

    implementing new produktionsmittel scheme

--- project/main_betriebe.py
+++ project/main_betriebe.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template, session, redirect, url_for, reques
 from flask_login import login_required, current_user
 from flask_table import LinkCol, Col
 from . import db
-from .models import Angebote, Kaeufe, Betriebe, Produktionsmittel
-from .forms import ProductSearchForm, ProductForm
-from .tables import Results, ProduktionsmittelTable, ProduktionsmittelTable2
+from .models import Angebote, Kaeufe, Betriebe, PMVerbrauchGesamt, PMVerbrauchProdukt
+from .forms import ProductSearchForm
+from .tables import Results, ProduktionsmittelTable
 from decimal import Decimal
+from sqlalchemy.sql.functions import coalesce
 
 main_betriebe = Blueprint('main_betriebe', __name__)
-
 
 @main_betriebe.route('/betriebe/home')
 def index():
@@ -36,15 +36,17 @@ def profile():
 @main_betriebe.route('/betriebe/produktionsmittel')
 @login_required
 def produktionsmittel():
-    produktionsmittel = db.session.execute(
-        "select kaeufe.id, angebote.name, angebote.beschreibung, angebote.preis,\
-        COALESCE(produktionsmittel.prozent_gebraucht, 0) prozent_gebraucht from kaeufe left join\
-        angebote on kaeufe.angebot=angebote.id left join produktionsmittel\
-        on kaeufe.id=produktionsmittel.kauf where kaeufe.betrieb=:param;", {"param":current_user.id}
-        )
+    produktionsmittel_qry = db.session.query(Kaeufe.id, Angebote.name, Angebote.beschreibung,\
+        Angebote.preis, coalesce(PMVerbrauchGesamt.prozent_gebraucht, 0).label("prozent_gebraucht")).\
+        outerjoin(Angebote, Kaeufe.angebot==Angebote.id).outerjoin(PMVerbrauchGesamt,\
+        Kaeufe.id==PMVerbrauchGesamt.kauf).filter(Kaeufe.betrieb==current_user.id)
 
-    table = ProduktionsmittelTable(produktionsmittel, classes=["table", "is-bordered", "is-striped"])
-    return render_template('produktionsmittel.html', table=table)
+    produktionsmittel_aktiv = produktionsmittel_qry.filter(PMVerbrauchGesamt.prozent_gebraucht < 100).all()
+    produktionsmittel_inaktiv = produktionsmittel_qry.filter(PMVerbrauchGesamt.prozent_gebraucht == 100).all()
+
+    table_aktiv = ProduktionsmittelTable(produktionsmittel_aktiv, classes=["table", "is-bordered", "is-striped"])
+    table_inaktiv = ProduktionsmittelTable(produktionsmittel_inaktiv, classes=["table", "is-bordered", "is-striped"])
+    return render_template('produktionsmittel.html', table_aktiv=table_aktiv, table_inaktiv=table_inaktiv)
 
 
 @main_betriebe.route('/betriebe/suchen', methods=['GET', 'POST'])
@@ -99,9 +101,8 @@ def kaufen(id):
             # angebote aktualisieren (aktiv = False)
             angebot.aktiv = False
             db.session.commit()
-            # produktionsmittel aktualisieren
-            new_produktionsmittel = Produktionsmittel(angebot=angebot.id,
-                    kauf=new_kauf.id, prozent_gebraucht=0)
+            # produktionsmittel (Verbrauch Gesamt) aktualisieren
+            new_produktionsmittel = PMVerbrauchGesamt(kauf=new_kauf.id, prozent_gebraucht=0)
             db.session.add(new_produktionsmittel)
             db.session.commit()
             # guthaben self aktualisieren
@@ -119,42 +120,27 @@ def kaufen(id):
 
 
 
-def save_changes(angebot, form, kosten_pm, new=False):
-    """
-    Save the changes to the database
-    """
-    angebot.name = form.name.data
-    angebot.betrieb = current_user.id
-    angebot.beschreibung = form.beschreibung.data
-    angebot.preis = Decimal(form.arbeit.data) + kosten_pm
-
-    if new:
-        db.session.add(angebot)
-
-    db.session.commit()
-
-
 @main_betriebe.route('/betriebe/anbieten', methods=['GET', 'POST'])
 @login_required
 def neues_angebot():
     """
     Ein neues Angebot hinzufügen
     """
-    form = ProductForm(request.form)
+    produktionsmittel_aktiv = db.session.query(Kaeufe.id, Angebote.name, Angebote.beschreibung,\
+        Angebote.preis, coalesce(PMVerbrauchGesamt.prozent_gebraucht, 0).label("prozent_gebraucht"), PMVerbrauchGesamt.id).\
+        outerjoin(Angebote, Kaeufe.angebot==Angebote.id).outerjoin(PMVerbrauchGesamt,\
+        Kaeufe.id==PMVerbrauchGesamt.kauf).filter(Kaeufe.betrieb==current_user.id, PMVerbrauchGesamt.prozent_gebraucht < 100).all()
 
-
-    if request.method == 'POST' and form.validate():
-        angebot = Angebote()
-
+    if request.method == 'POST':
         # create request dictionary
         request_dict = request.form.to_dict()
-        print(request_dict)
         # dict with produktionsmittel values
         pm_dict = dict(filter(lambda elem: elem[0][:3] == 'id_', request_dict.items()))
         # pm dict entries that are not zero
         pm_dict_not_zero = dict(filter(lambda elem: Decimal(elem[1]) != 0,pm_dict.items()))
+        kosten_pm = 0
         if pm_dict_not_zero:
-            # get list of ids, prozente
+            # calculate produktionsmittelkosten
             id_list = []
             prozent_list = []
             for key in list(pm_dict_not_zero.keys()):
@@ -166,45 +152,33 @@ def neues_angebot():
             for idx in id_list:
                 qry = db.session.query(Kaeufe, Angebote.preis).join(Angebote, Kaeufe.angebot==Angebote.id).filter(Kaeufe.id == idx).all()
                 preise_list.append(Decimal(qry[0][1]))
-            print("id_list", id_list)
-            print("prozent_list", prozent_list)
-            print("preise_list", preise_list)
             kosten_einzeln = []
             for num1, num2 in zip(prozent_list, preise_list):
                 kosten_einzeln.append(num1 * num2)
-            print("einzelne kosten:", kosten_einzeln)
             kosten_pm = sum(kosten_einzeln)
-            print("kosten_gesamt", kosten_pm)
+            # update prodmittel gebraucht (gesamt) in prozent
+            assert len(id_list) == len(prozent_list)
+            for count, i in enumerate(id_list):
+                prdmittel = db.session.query(PMVerbrauchGesamt).filter(PMVerbrauchGesamt.kauf == i).first()
+                prdmittel.prozent_gebraucht += prozent_list[count]*100
+                db.session.commit()
+                print("prozent_gebraucht gesamt updated!")
 
-            # Prozent_Gebraucht anpassen!
-            # wenn prozent_gebraucht == 100 --> aus produktionsmittelansicht ausschließen
+        # save new angebot
+        new_angebot = Angebote(name=request.form["name"], betrieb=current_user.id,\
+            beschreibung=request.form["beschreibung"], preis=Decimal(request.form["arbeit"]) + kosten_pm)
+        db.session.add(new_angebot)
+        db.session.commit()
 
+        if pm_dict_not_zero:
+            # create prdmittel gebraucht (Produkt) in prozent
+            assert len(id_list) == len(prozent_list)
+            for count, i in enumerate(id_list):
+                new_produktionsmittel_prd = PMVerbrauchProdukt(angebot=new_angebot.id, kauf=i, prozent_gebraucht=prozent_list[count]*100)
+                db.session.add(new_produktionsmittel_prd)
+                db.session.commit()
 
-
-        save_changes(angebot, form, kosten_pm, new=True)
         flash('Angebot erfolgreich gespeichert!')
         return redirect('/betriebe/home')
 
-
-    produktionsmittel = db.session.execute(
-        "select kaeufe.id, angebote.name, angebote.beschreibung, angebote.preis,\
-        COALESCE(produktionsmittel.prozent_gebraucht, 0) prozent_gebraucht from kaeufe left join\
-        angebote on kaeufe.angebot=angebote.id left join produktionsmittel\
-        on kaeufe.id=produktionsmittel.kauf where kaeufe.betrieb=:param;", {"param":current_user.id}
-        )
-
-    # creates dict out of resultproxy
-    d, a = {}, []
-    for rowproxy in produktionsmittel:
-        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
-        for column, value in rowproxy.items():
-            # build up the dictionary
-            d = {**d, **{column: value}}
-        a.append(d)
-
-    # creates list of [names, prozent, preis, kaeufe-id]
-    produkt_mittel_names = []
-    for item in a:
-        # produkt_mittel_names.append([item["name"], item["prozent_gebraucht"], item["preis"], item["id"]])
-        produkt_mittel_names.append([item["id"], item["name"], item["beschreibung"], item["preis"], item["prozent_gebraucht"]])
-    return render_template('neues_angebot.html', form=form, produkt_mittel_names=produkt_mittel_names)
+    return render_template('neues_angebot.html', produktionsmittel_aktiv=produktionsmittel_aktiv)
