51997825525ece6a00ba0121ce8ecae9819fe22b
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-08 16:17:41 +0100

    tables

--- project/tables.py
+++ project/tables.py
@@ -1,12 +1,5 @@
 from flask_table import Table, Col
 
-# class Results(Table):
-#     id = Col('Id', show=False)
-#     name = Col('Name')
-#     name = Col('Name Betrieb')
-#     beschreibung = Col('Beschreibung')
-#     preis = Col('Preis')
-
 class ProduktionsmittelTable(Table):
     id = Col('Kauf-Id')
     name = Col('Name')
@@ -17,3 +10,4 @@ class ProduktionsmittelTable(Table):
 class ArbeiterTable(Table):
     id = Col('Nutzer-Id')
     name = Col('Name')
+    summe_stunden = Col('Summe Stunden')
