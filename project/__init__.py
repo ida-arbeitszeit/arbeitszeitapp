b0b3c0c7481b89681bffbd142586ac30e79b041c
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-09 20:01:39 +0100

    init

--- project/__init__.py
+++ project/__init__.py
@@ -3,14 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import project.my_database_key as db_key
 
-
-# init SQLAlchemy so we can use it later in our models
+# init SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SECRET_KEY'] = 'dev'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_key.username}:{db_key.password}@localhost/betriebe2'
 
@@ -27,16 +26,13 @@ def create_app():
     def load_user(user_id):
         user_type = session["user_type"]
         if user_type == "nutzer":
-            # since the user_id is just the primary key of our user table, use it in the query for the user
             return Nutzer.query.get(int(user_id))
         elif user_type == "betrieb":
             return Betriebe.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
     from .main_nutzer import main_nutzer as main_nutzer_blueprint
     app.register_blueprint(main_nutzer_blueprint)
 
