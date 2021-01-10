c5d10a4639ab2f395c4971628e95d69479f90d10
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-10 22:27:55 +0100

    change import

--- project/auth.py
+++ project/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import Nutzer, Betriebe
-from . import db
+from .app import db
 
 auth = Blueprint('auth', __name__)
 
