4b8f287b38371e0f910efe4ebf4463ffc96a883a
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-11 18:28:07 +0100

    back to flask factory in __init__

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import project.my_database_key as db_key
+from flask_table import Table, Col
 
 # init SQLAlchemy
 db = SQLAlchemy()
@@ -9,33 +9,35 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = 'dev'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_key.username}:{db_key.password}@localhost/betriebe2'
+    app.config.from_object('config.Config')
     login_manager.login_view = 'auth.start'
 
-    login_manager.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
-    from .models import Nutzer
-    from .models import Betriebe
+    with app.app_context():
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        user_type = session["user_type"]
-        if user_type == "nutzer":
-            return Nutzer.query.get(int(user_id))
-        elif user_type == "betrieb":
-            return Betriebe.query.get(int(user_id))
+        from .models import Nutzer
+        from .models import Betriebe
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+        @login_manager.user_loader
+        def load_user(user_id):
+         user_type = session["user_type"]
+         if user_type == "nutzer":
+             return Nutzer.query.get(int(user_id))
+         elif user_type == "betrieb":
+             return Betriebe.query.get(int(user_id))
 
-    from .main_nutzer import main_nutzer as main_nutzer_blueprint
-    app.register_blueprint(main_nutzer_blueprint)
+        from .auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint)
 
-    from .main_betriebe import main_betriebe as main_betriebe_blueprint
-    app.register_blueprint(main_betriebe_blueprint)
+        from .main_nutzer import main_nutzer as main_nutzer_blueprint
+        app.register_blueprint(main_nutzer_blueprint)
 
-    return app
+        from .main_betriebe import main_betriebe as main_betriebe_blueprint
+        app.register_blueprint(main_betriebe_blueprint)
+
+        # Create Database Models
+        db.create_all()
+
+        return app
