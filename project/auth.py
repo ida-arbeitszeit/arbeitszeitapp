afc55e31ff4cebefd7dba863c45d2a78c5077a20
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-11 19:10:51 +0100

    changing import statements in auth and models

--- project/auth.py
+++ project/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import Nutzer, Betriebe
-from .app import db
+from . import db
 
 
 
--- project/models.py
+++ project/models.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-from .app import db
+from . import db
 
 class Nutzer(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
