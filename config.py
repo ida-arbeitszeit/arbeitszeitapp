2f6a92eb24957512e47280fa4996897bc3206942
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-11 22:10:15 +0100

    changes to config

--- config.py
+++ config.py
@@ -1,19 +1,42 @@
 """Flask configuration."""
-# import project.my_database_key as db_key
+from os import environ, path
+from dotenv import load_dotenv
 
-# from os import environ, path
-# from dotenv import load_dotenv
-#
-# basedir = path.abspath(path.dirname(__file__))
-# load_dotenv(path.join(basedir, '.env'))
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+
+class Config:
+    """Base config."""
+    SECRET_KEY = b'\xb1:\x7f/X\xfd\x9e\xe4)`\xc0\x88\x80\xad\xecK'
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+
+
+class ProdConfig(Config):
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = "postgres://vafeqodbfvitgv:90a92c73899e009283cc949375ad7d6a7f25f39ee122a20ca6818396eac6b098@ec2-52-211-108-161.eu-west-1.compute.amazonaws.com:5432/d9vli47sdpq0if"
+
+
+class DevConfig(Config):
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI = environ.get('DEV_DATABASE_URI')
 
 
 class Config:
     """Set Flask config variables."""
 
     # FLASK_ENV = 'development'
-    # TESTING = True
-    SECRET_KEY = b'\xb1:\x7f/X\xfd\x9e\xe4)`\xc0\x88\x80\xad\xecK'
+    TESTING = True
+    DEBUG = True
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
     # SECRET_KEY = environ.get('SECRET_KEY')
     # STATIC_FOLDER = 'static'
     # TEMPLATES_FOLDER = 'templates'
@@ -21,10 +44,4 @@ class Config:
     # Database
     #SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = "postgres://vafeqodbfvitgv:90a92c73899e009283cc949375ad7d6a7f25f39ee122a20ca6818396eac6b098@ec2-52-211-108-161.eu-west-1.compute.amazonaws.com:5432/d9vli47sdpq0if"
-    # SQLALCHEMY_DATABASE_URI = f'postgresql://{db_key.username}:{db_key.password}@localhost/betriebe2'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # AWS Secrets
-    # AWS_SECRET_KEY = environ.get('AWS_SECRET_KEY')
-    # AWS_KEY_ID = environ.get('AWS_KEY_ID')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
