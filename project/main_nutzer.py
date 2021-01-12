5c07b244e654506e3cc18072a270b572bbaff648
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-12 19:11:25 +0100

    main nutzer

--- project/main_nutzer.py
+++ project/main_nutzer.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from . import db
-from .models import Angebote, Kaeufe, Nutzer, Betriebe, Arbeit
+from .models import Angebote, Kaeufe, Nutzer, Betriebe, Arbeit, Arbeiter
 from .forms import ProductSearchForm
+from .tables import KaeufeTable, ArbeitsstellenTable
 
 
 main_nutzer = Blueprint('main_nutzer', __name__)
@@ -20,6 +21,26 @@ def index():
     else:
         session["user_type"] = "nutzer"
         return render_template('index_nutzer.html')
+
+
+@main_nutzer.route('/nutzer/kaeufe')
+@login_required
+def meine_kaeufe():
+    try:
+        user_type = session["user_type"]
+    except:
+        user_type = "nutzer"
+
+    if user_type == "betrieb":
+        return redirect(url_for('auth.zurueck'))
+    else:
+        session["user_type"] = "nutzer"
+
+        kaufhistorie = db.session.query(Kaeufe.id, Angebote.name, Angebote.beschreibung, Angebote.preis).select_from(Kaeufe).\
+            filter_by(nutzer=current_user.id).join(Angebote, Kaeufe.angebot==Angebote.id).all()
+        kaufh_table = KaeufeTable(kaufhistorie)
+        return render_template('meine_kaeufe.html', kaufh_table=kaufh_table)
+
 
 
 @main_nutzer.route('/nutzer/suchen', methods=['GET', 'POST'])
@@ -125,13 +146,18 @@ def kaufen(id):
         return 'Error loading #{id}'.format(id=id)
 
 
-
-
 @main_nutzer.route('/nutzer/profile')
 @login_required
 def profile():
     user_type = session["user_type"]
     if user_type == "nutzer":
-        return render_template('profile_nutzer.html')
+        arbeitsstellen = db.session.query(Betriebe.name).select_from(Arbeiter).\
+            filter_by(nutzer=current_user.id).join(Betriebe, Arbeiter.betrieb==Betriebe.id).all()
+        if arbeitsstellen:
+            arbeitsstellen_table = ArbeitsstellenTable(arbeitsstellen)
+        else:
+            arbeitsstellen_table = None
+
+        return render_template('profile_nutzer.html', arbeitsstellen_table=arbeitsstellen_table)
     elif user_type == "betrieb":
         return redirect(url_for('auth.zurueck'))
