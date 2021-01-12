d2d4d6cd439f9dec717dde2e61fd45d8d255dfef
sebastianloschert <sebastian.loschert@gmx.de>
2021-01-12 11:52:23 +0100

    secret key and database with env variables

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv(path.join(basedir, '.env'))
 
 class Config:
     """Base config."""
-    SECRET_KEY = b'\xb1:\x7f/X\xfd\x9e\xe4)`\xc0\x88\x80\xad\xecK'
+    SECRET_KEY = os.environ('MY_SECRET_KEY')
+    #SECRET_KEY = b'\xb1:\x7f/X\xfd\x9e\xe4)`\xc0\x88\x80\xad\xecK'
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -17,7 +18,7 @@ class ProdConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "postgres://vafeqodbfvitgv:90a92c73899e009283cc949375ad7d6a7f25f39ee122a20ca6818396eac6b098@ec2-52-211-108-161.eu-west-1.compute.amazonaws.com:5432/d9vli47sdpq0if"
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 
 
 class DevConfig(Config):
