752f29dd761f54ce65bdc768f52afad1a10b56d8
sebas <sebastian.loschert@gmx.de>
2021-05-08 17:59:35 +0200

    simplifying around suchen, kaufen and composition of prices.

--- project/suchen_und_kaufen.py
+++ project/suchen_und_kaufen.py
@@ -1,14 +1,19 @@
-from .forms import ProductSearchForm
-from .models import Angebote, Betriebe, KooperationenMitglieder
+"""SQL-requests around searching and buying products."""
+
+import datetime
+from flask import render_template, redirect, request, flash
 from . import db
+from .forms import ProductSearchForm
+from .models import Angebote, Betriebe, KooperationenMitglieder, Nutzer, Kaeufe, Arbeit
 from sqlalchemy import text
 from sqlalchemy.sql import func, case
 from sqlalchemy.orm import aliased
-from flask import render_template, redirect, request, flash
 
 
 def get_angebote():
     """
+    search products.
+
     returns all products available (grouped results, active or not),
     with several columns, including the coop-price.
     """
@@ -48,6 +53,8 @@ def get_angebote():
 
 def such_vorgang(suchender_type, request_form):
     """
+    search products.
+
     returns html pages with search results
     """
 
@@ -89,3 +96,53 @@ def such_vorgang(suchender_type, request_form):
             return render_template(render_dir, form=search, results=results)
 
     return render_template(render_dir, form=search, results=results)
+
+
+
+def kauf_vorgang(kaufender_type, angebot, kaeufer_id):
+    """
+    buy product.
+    """
+    # aktuellen (koop-)preis erhalten:
+    koop = db.session.query(KooperationenMitglieder).join(Angebote).\
+        filter(Angebote.id == angebot.id, Angebote.aktiv == angebot.aktiv).first()
+    if not koop:
+        preis = angebot.preis
+    else:
+        preis = db.session.query(func.avg(Angebote.preis)).\
+            select_from(KooperationenMitglieder).\
+            join(Angebote).\
+            filter(Angebote.aktiv == True).\
+            filter(KooperationenMitglieder.kooperation == koop.kooperation).\
+            group_by(KooperationenMitglieder.kooperation).scalar()
+
+    # kauefe aktualisieren
+    if kaufender_type == "betriebe":
+        kaufender = Betriebe
+        new_kauf = Kaeufe(kauf_date = datetime.datetime.now(), angebot = angebot.id,
+                type_nutzer = False, betrieb = kaeufer_id,
+                nutzer = None, kaufpreis=preis)
+        db.session.add(new_kauf)
+        db.session.commit()
+    elif kaufender_type == "nutzer":
+        kaufender = Nutzer
+        new_kauf = Kaeufe(kauf_date = datetime.datetime.now(), angebot = angebot.id,
+                type_nutzer = True, betrieb = None,
+                nutzer = kaeufer_id, kaufpreis=preis)
+        db.session.add(new_kauf)
+        db.session.commit()
+
+    # angebote aktiv = False
+    angebot.aktiv = False
+    db.session.commit()
+
+    # guthaben käufer verringern
+    kaeufer = db.session.query(kaufender).filter(kaufender.id == kaeufer_id).first()
+    kaeufer.guthaben -= preis
+    db.session.commit()
+
+    # guthaben des anbietenden betriebes erhöhen
+    anbietender_betrieb_id = angebot.betrieb
+    anbietender_betrieb = Betriebe.query.filter_by(id=anbietender_betrieb_id).first()
+    anbietender_betrieb.guthaben += preis # angebot.p_kosten
+    db.session.commit()
