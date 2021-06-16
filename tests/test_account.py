c655d1be2cbb5b6a71a157c096b098684c174908
sebas <sebastian.loschert@gmx.de>
2021-06-16 23:25:01 +0200

    fixing bug

--- tests/test_account.py
+++ tests/test_account.py
@@ -7,6 +7,6 @@ from tests.dependency_injection import injection_test
 def test_account_balance_changes(
     account_generator: AccountGenerator,
 ):
-    account = account_generator.create_account(balance=20)
+    account = account_generator.create_account()
     adjust_balance(account, -5)
-    assert account.balance == 15
+    assert account.balance == -5
