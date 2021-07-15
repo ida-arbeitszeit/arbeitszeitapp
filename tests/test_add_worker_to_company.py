c2c1d41d646871a7c6b55cd1b83a6b3450f52b3f
sebas <sebastian.loschert@gmx.de>
2021-07-15 22:48:08 +0200

    tests for use_case add_worker_to_company

---
+++ tests/test_add_worker_to_company.py
@@ -0,0 +1,57 @@
+import pytest
+from arbeitszeit.use_cases import add_worker_to_company
+from tests.dependency_injection import injection_test
+from tests.repositories import CompanyWorkerRepository
+from tests.data_generators import CompanyGenerator, MemberGenerator
+from arbeitszeit.errors import (
+    CompanyDoesNotExist,
+    WorkerDoesNotExist,
+    WorkerAlreadyAtCompany,
+)
+
+
+@injection_test
+def test_that_error_is_raised_if_nonexisting_company(
+    company_worker_repository: CompanyWorkerRepository,
+    member_generator: MemberGenerator,
+):
+    company = None
+    worker = member_generator.create_member()
+    with pytest.raises(CompanyDoesNotExist):
+        add_worker_to_company(company_worker_repository, company, worker)
+
+
+@injection_test
+def test_that_error_is_raised_if_nonexisting_worker(
+    company_worker_repository: CompanyWorkerRepository,
+    company_generator: CompanyGenerator,
+):
+    company = company_generator.create_company()
+    worker = None
+    with pytest.raises(WorkerDoesNotExist):
+        add_worker_to_company(company_worker_repository, company, worker)
+
+
+@injection_test
+def test_that_error_is_raised_if_worker_already_at_company(
+    company_worker_repository: CompanyWorkerRepository,
+    company_generator: CompanyGenerator,
+    member_generator: MemberGenerator,
+):
+    company = company_generator.create_company()
+    worker = member_generator.create_member()
+    company_worker_repository.add_worker_to_company(company, worker)
+    with pytest.raises(WorkerAlreadyAtCompany):
+        add_worker_to_company(company_worker_repository, company, worker)
+
+
+@injection_test
+def test_that_worker_is_added_to_company_worker_repo(
+    company_worker_repository: CompanyWorkerRepository,
+    company_generator: CompanyGenerator,
+    member_generator: MemberGenerator,
+):
+    company = company_generator.create_company()
+    worker = member_generator.create_member()
+    add_worker_to_company(company_worker_repository, company, worker)
+    assert worker in company.workers
