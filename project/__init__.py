e87cbc03719d835f1d84f549d7deedfb1ae1cf62
sebas <sebastian.loschert@gmx.de>
2021-07-28 22:47:24 +0200

    newly created revision file

--- project/__init__.py
+++ project/__init__.py
@@ -42,6 +42,6 @@ def create_app():
         app.register_blueprint(member_routes.main_member)
 
         # create the initial database
-        db.create_all()
+        # db.create_all()
 
         return app
