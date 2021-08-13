39a0453e0530334cce6677314bc266e17eff9d08
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-08-13 15:22:26 +0200

    Implement more tests for MemberRepository

--- tests/fask_integration/test_member_repository.py
+++ tests/fask_integration/test_member_repository.py
@@ -1,5 +1,10 @@
+from uuid import uuid4
+
+import pytest
+
 from arbeitszeit.entities import AccountTypes
 from project.database.repositories import AccountRepository, MemberRepository
+from project.error import MemberNotFound
 from tests.dependency_injection import injection_test
 
 
@@ -17,3 +22,22 @@ def test_that_users_can_be_converted_from_and_to_orm_objects(
         )
     )
     assert converted_member == expected_member
+
+
+@injection_test
+def test_that_members_cannot_be_retrieved_when_db_is_empty(
+    member_repository: MemberRepository,
+):
+    with pytest.raises(MemberNotFound):
+        member_repository.get_member_by_id(uuid4())
+
+
+@injection_test
+def test_cannot_find_member_by_email_before_it_was_added(
+    member_repository: MemberRepository,
+    account_repository: AccountRepository,
+):
+    assert not member_repository.has_member_with_email("member@cp.org")
+    account = account_repository.create_account(AccountTypes.member)
+    member_repository.create_member("member@cp.org", "karl", "password", account)
+    assert member_repository.has_member_with_email("member@cp.org")
