7953c55cf6e278b546f110fc9d6178172ac78e5e
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-09-11 13:25:48 +0200

    Typo and additional tests for QueryProductsPresenter

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((html-mode . ((sgml-basic-offset . 4)))
+ (js-mode . ((tab-width . 4)
+	     (js-indent-level . 4))))
--- tests/presenters/test_query_products_presenter.py
+++ tests/presenters/test_query_products_presenter.py
@@ -5,14 +5,29 @@ from uuid import uuid4
 from arbeitszeit.use_cases.query_products import ProductQueryResponse, QueriedProduct
 from arbeitszeit_web.query_products import QueryProductsPresenter
 
+RESPONSE_WITHOUT_RESULTS = ProductQueryResponse(results=[])
+RESPONSE_WITH_ONE_RESULT = ProductQueryResponse(
+    results=[
+        QueriedProduct(
+            offer_id=uuid4(),
+            seller_name="Seller name",
+            seller_email="seller@cp.org",
+            plan_id=uuid4(),
+            product_name="Bread",
+            product_description="For eating",
+            price_per_unit=Decimal(0.001),
+            is_public_service=True,
+        )
+    ]
+)
+
 
 class QueryProductsPresenterTests(TestCase):
     def setUp(self):
         self.presenter = QueryProductsPresenter()
 
     def test_presenting_empty_response_leads_to_not_showing_results(self):
-        use_case_response = ProductQueryResponse(results=[])
-        presentation = self.presenter.present(use_case_response)
+        presentation = self.presenter.present(RESPONSE_WITHOUT_RESULTS)
         self.assertFalse(presentation.show_results)
 
     def test_empty_view_model_does_not_show_results(self):
@@ -20,19 +35,13 @@ class QueryProductsPresenterTests(TestCase):
         self.assertFalse(presentation.show_results)
 
     def test_non_empty_use_case_response_leads_to_showing_results(self):
-        use_case_response = ProductQueryResponse(
-            results=[
-                QueriedProduct(
-                    offer_id=uuid4(),
-                    seller_name="Seller name",
-                    seller_email="seller@cp.org",
-                    plan_id=uuid4(),
-                    product_name="Bread",
-                    product_description="For eating",
-                    price_per_unit=Decimal(0.001),
-                    is_public_service=True,
-                )
-            ]
-        )
-        presentation = self.presenter.present(use_case_response)
+        presentation = self.presenter.present(RESPONSE_WITH_ONE_RESULT)
         self.assertTrue(presentation.show_results)
+
+    def test_show_notification_when_no_results_are_found(self):
+        presentation = self.presenter.present(RESPONSE_WITHOUT_RESULTS)
+        self.assertTrue(presentation.notifications)
+
+    def test_dont_show_notifications_when_results_are_found(self):
+        presentation = self.presenter.present(RESPONSE_WITH_ONE_RESULT)
+        self.assertFalse(presentation.notifications)
