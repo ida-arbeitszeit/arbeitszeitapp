3fb9bf3f35419240b5cc1b0abaacc73d3e088403
Michael Schmidt <github@schmidt.tw>
2021-09-22 16:51:22 +0200

    Add seasurf CSRF protection

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, session
 from flask_table import Col, Table  # noqa: Do not delete
 from flask_talisman import Talisman
+from flask_seasurf import SeaSurf
 
 import project.extensions
 from project.extensions import login_manager
@@ -30,6 +31,7 @@ def create_app(config=None, db=None, migrate=None):
         Talisman(app, content_security_policy=csp)
 
     # init flask extensions
+    SeaSurf(app)
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
