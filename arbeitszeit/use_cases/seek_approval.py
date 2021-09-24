f34059f17b3bd3b5819f5ddeb0dcb178763e5429
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-09-24 13:15:11 +0200

    Refactor SeekApproval

--- arbeitszeit/use_cases/seek_approval.py
+++ arbeitszeit/use_cases/seek_approval.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Optional
 from uuid import UUID
 
 from injector import inject
 
 from arbeitszeit.datetime_service import DatetimeService
+from arbeitszeit.entities import Plan
 from arbeitszeit.repositories import PlanRepository
 
 
@@ -23,25 +25,32 @@ class SeekApproval:
     def __call__(
         self, new_plan_id: UUID, original_plan_id: Optional[UUID]
     ) -> SeekApprovalResponse:
-        """
-        Company seeks plan approval. Either for a new plan or for a plan reneweal.
-        Sets approved either to True or False, sets approval_date and approval_reason.
+        """Company seeks plan approval. Either for a new plan or for a plan
+        reneweal.  Sets approved either to True or False, sets
+        approval_date and approval_reason.
 
-        Additionally, if it's a plan renewal, the original plan will be set to "renewed".
-        Returns the approval decision.
+        Additionally, if it's a plan renewal, the original plan will
+        be set to "renewed".  Returns the approval decision.
         """
-        new_plan = self.plan_repository.get_plan_by_id(new_plan_id)
-        original_plan = (
-            None
-            if original_plan_id is None
-            else self.plan_repository.get_plan_by_id(original_plan_id)
-        )
-        approval_date = self.datetime_service.now()
-        self.plan_repository.approve_plan(new_plan, approval_date)
-        if original_plan:
-            self.plan_repository.renew_plan(original_plan)
-        assert new_plan.approval_reason
+        plan = self.plan_repository.get_plan_by_id(new_plan_id)
+        if original_plan_id:
+            reason = self.renew_plan(plan, original_plan_id)
+        else:
+            reason = self.approve_plan(plan)
         return SeekApprovalResponse(
             is_approved=True,
-            reason=new_plan.approval_reason,
+            reason=reason,
         )
+
+    def renew_plan(self, plan: Plan, original_plan_id: UUID) -> str:
+        original_plan = self.plan_repository.get_plan_by_id(original_plan_id)
+        self.plan_repository.renew_plan(original_plan)
+        return self.approve_plan(plan)
+
+    def approve_plan(self, plan: Plan) -> str:
+        self.plan_repository.approve_plan(plan, self.get_approval_date())
+        assert plan.approval_reason
+        return plan.approval_reason
+
+    def get_approval_date(self) -> datetime:
+        return self.datetime_service.now()
