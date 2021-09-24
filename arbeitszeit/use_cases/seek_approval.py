12fa39ec6476d66d090b436d53a90e7cbebacb06
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-09-24 13:34:20 +0200

    Remove redundant comment

--- arbeitszeit/use_cases/seek_approval.py
+++ arbeitszeit/use_cases/seek_approval.py
@@ -25,13 +25,6 @@ class SeekApproval:
     def __call__(
         self, new_plan_id: UUID, original_plan_id: Optional[UUID]
     ) -> SeekApprovalResponse:
-        """Company seeks plan approval. Either for a new plan or for a plan
-        reneweal.  Sets approved either to True or False, sets
-        approval_date and approval_reason.
-
-        Additionally, if it's a plan renewal, the original plan will
-        be set to "renewed".  Returns the approval decision.
-        """
         plan = self.plan_repository.get_plan_by_id(new_plan_id)
         if original_plan_id:
             reason = self.renew_plan(plan, original_plan_id)
