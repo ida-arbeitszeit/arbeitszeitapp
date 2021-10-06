38b245fdddf91c8973d23257305fa77ee484c55b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-10-06 19:55:07 +0200

    Use canonical ViewTestCase in tests for GetPlanSummary view

--- tests/flask_integration/test_get_plan_summary.py
+++ tests/flask_integration/test_get_plan_summary.py
@@ -1,20 +1,15 @@
 from uuid import uuid4
 
-from tests.data_generators import CompanyGenerator, MemberGenerator, PlanGenerator
+from tests.data_generators import PlanGenerator
 
-from .dependency_injection import ViewTestCase
+from .view import ViewTestCase
 
 
 class AuthenticatedMemberTests(ViewTestCase):
     def setUp(self) -> None:
         super().setUp()
-        member_generator = self.injector.get(MemberGenerator)
         self.plan_generator = self.injector.get(PlanGenerator)
-        self.member_password = "password123"
-        self.current_member = member_generator.create_member(
-            password=self.member_password
-        )
-        self.login()
+        self.login_member()
 
     def test_get_404_when_plan_does_not_exist(self) -> None:
         url = f"/member/plan_summary/{uuid4()}"
@@ -27,28 +22,12 @@ class AuthenticatedMemberTests(ViewTestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def login(self):
-        response = self.client.post(
-            "/member/login",
-            data=dict(
-                email=self.current_member.email,
-                password=self.member_password,
-            ),
-            follow_redirects=True,
-        )
-        assert response.status_code < 400
-
 
 class AuthenticatedCompanyTests(ViewTestCase):
     def setUp(self) -> None:
         super().setUp()
-        company_generator = self.injector.get(CompanyGenerator)
         self.plan_generator = self.injector.get(PlanGenerator)
-        self.company_password = "password123"
-        self.current_company = company_generator.create_company(
-            password=self.company_password
-        )
-        self.login()
+        self.login_company()
 
     def test_get_404_when_plan_does_not_exist(self) -> None:
         url = f"/company/plan_summary/{uuid4()}"
@@ -60,14 +39,3 @@ class AuthenticatedCompanyTests(ViewTestCase):
         url = f"/company/plan_summary/{plan.id}"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-
-    def login(self):
-        response = self.client.post(
-            "/company/login",
-            data=dict(
-                email=self.current_company.email,
-                password=self.company_password,
-            ),
-            follow_redirects=True,
-        )
-        assert response.status_code < 400
