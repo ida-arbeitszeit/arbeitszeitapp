299bfdd7e8d0c90af6fae0d67881253d3db25f1f
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-01-05 20:48:18 +0100

    When answering work invite check if answering member was actually invited

--- arbeitszeit/use_cases/answer_company_work_invite.py
+++ arbeitszeit/use_cases/answer_company_work_invite.py
@@ -3,13 +3,18 @@ from uuid import UUID
 
 from injector import inject
 
-from arbeitszeit.repositories import CompanyWorkerRepository, WorkerInviteRepository
+from arbeitszeit.repositories import (
+    CompanyWorkerRepository,
+    MemberRepository,
+    WorkerInviteRepository,
+)
 
 
 @dataclass
 class AnswerCompanyWorkInviteRequest:
     is_accepted: bool
     invite_id: UUID
+    user: UUID
 
 
 @dataclass
@@ -22,12 +27,15 @@ class AnswerCompanyWorkInviteResponse:
 class AnswerCompanyWorkInvite:
     worker_invite_repository: WorkerInviteRepository
     company_worker_repository: CompanyWorkerRepository
+    member_repository: MemberRepository
 
     def __call__(
         self, request: AnswerCompanyWorkInviteRequest
     ) -> AnswerCompanyWorkInviteResponse:
         invite = self.worker_invite_repository.get_by_id(request.invite_id)
         if invite is None:
+            return AnswerCompanyWorkInviteResponse(is_success=False)
+        if invite.member.id != request.user:
             return AnswerCompanyWorkInviteResponse(is_success=False)
         elif request.is_accepted:
             self.company_worker_repository.add_worker_to_company(
--- tests/use_cases/test_answer_company_work_invite.py
+++ tests/use_cases/test_answer_company_work_invite.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from unittest import TestCase
 from uuid import UUID, uuid4
 
@@ -27,14 +28,14 @@ class AnwerCompanyWorkInviteTests(TestCase):
 
     def test_trying_to_answer_non_existing_invite_is_unsuccessful(self) -> None:
         response = self.answer_company_work_invite(
-            AnswerCompanyWorkInviteRequest(is_accepted=True, invite_id=uuid4())
+            self._create_request(is_accepted=True, invite_id=uuid4())
         )
         self.assertFalse(response.is_success)
 
     def test_rejecting_existing_invite_is_successful(self) -> None:
         invite_id = self._invite_worker()
         response = self.answer_company_work_invite(
-            AnswerCompanyWorkInviteRequest(
+            self._create_request(
                 is_accepted=False,
                 invite_id=invite_id,
             )
@@ -44,7 +45,7 @@ class AnwerCompanyWorkInviteTests(TestCase):
     def test_accepting_an_invite_adds_member_to_company_workers(self) -> None:
         invite_id = self._invite_worker()
         response = self.answer_company_work_invite(
-            AnswerCompanyWorkInviteRequest(
+            self._create_request(
                 is_accepted=True,
                 invite_id=invite_id,
             )
@@ -56,7 +57,7 @@ class AnwerCompanyWorkInviteTests(TestCase):
     def test_rejecting_an_invite_does_not_add_member_to_company_workers(self) -> None:
         invite_id = self._invite_worker()
         response = self.answer_company_work_invite(
-            AnswerCompanyWorkInviteRequest(
+            self._create_request(
                 is_accepted=False,
                 invite_id=invite_id,
             )
@@ -70,12 +71,39 @@ class AnwerCompanyWorkInviteTests(TestCase):
     def test_invite_gets_deleted_after_rejecting_an_invite(self) -> None:
         invite_id = self._invite_worker()
         self.answer_company_work_invite(
-            AnswerCompanyWorkInviteRequest(
+            self._create_request(
                 is_accepted=False,
                 invite_id=invite_id,
             )
         )
         self.assertIsNone(self.invite_repository.get_by_id(invite_id))
+
+    def test_answer_invite_as_non_existing_member_returns_negative_reponse(
+        self,
+    ) -> None:
+        invite_id = self._invite_worker()
+        response = self.answer_company_work_invite(
+            self._create_request(
+                is_accepted=True,
+                invite_id=invite_id,
+                user=uuid4(),
+            )
+        )
+        self.assertFalse(response.is_success)
+
+    def test_answer_invite_as_member_that_was_not_invited_returns_negative_reponse(
+        self,
+    ) -> None:
+        invite_id = self._invite_worker()
+        other_member = self.member_generator.create_member()
+        response = self.answer_company_work_invite(
+            self._create_request(
+                is_accepted=True,
+                invite_id=invite_id,
+                user=other_member.id,
+            )
+        )
+        self.assertFalse(response.is_success)
 
     def _invite_worker(self) -> UUID:
         invite_response = self.invite_worker_to_company(
@@ -87,3 +115,14 @@ class AnwerCompanyWorkInviteTests(TestCase):
         invite_id = invite_response.invite_id
         assert invite_id
         return invite_id
+
+    def _create_request(
+        self, invite_id: UUID, is_accepted: bool, user: Optional[UUID] = None
+    ) -> AnswerCompanyWorkInviteRequest:
+        if user is None:
+            user = self.member.id
+        return AnswerCompanyWorkInviteRequest(
+            invite_id=invite_id,
+            is_accepted=is_accepted,
+            user=user,
+        )
