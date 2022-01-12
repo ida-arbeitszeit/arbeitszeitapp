43133345d1d6170b2dd9183cc26fd747c5e5dd55
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-01-12 10:28:48 +0100

    Fix MessageRepository

--- project/models.py
+++ project/models.py
@@ -186,9 +186,15 @@ class Message(db.Model):
     addressee = db.Column(db.String)
     title = db.Column(db.String)
     content = db.Column(db.String)
-    user_action = db.Column(db.Enum(UserActionType), nullable=True)
     sender_remarks = db.Column(db.String, nullable=True)
     is_read = db.Column(db.Boolean)
+    user_action = db.Column(db.String, db.ForeignKey("user_action.id"), nullable=True)
+
+
+class UserAction(db.Model):
+    id = db.Column(db.String, primary_key=True, default=generate_uuid)
+    reference = db.Column(db.String)
+    action_type = db.Column(db.Enum(UserActionType))
 
 
 class Cooperation(db.Model):
