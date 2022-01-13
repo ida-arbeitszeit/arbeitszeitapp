52a1f3212c2fb8c2d00a1dc9577df33cce7223a1
sebas <sebastian.loschert@gmx.de>
2022-01-13 14:39:27 +0100

    adapting migration file for sqlite

--- migrations/versions/fb89625b2d12_add_registered_on_and_confirmed_on_to_.py
+++ migrations/versions/fb89625b2d12_add_registered_on_and_confirmed_on_to_.py
@@ -20,12 +20,14 @@ def upgrade():
     # set default values for registered_on and confirmed_on
     op.add_column("company", sa.Column("registered_on", sa.DateTime(), nullable=True))
     op.execute("UPDATE company SET registered_on = '2021-12-21 00:00:00.000'")
-    op.alter_column("company", "registered_on", nullable=False)
+    with op.batch_alter_table("company") as batch_op:
+        batch_op.alter_column("registered_on", nullable=False)
 
     op.add_column("company", sa.Column("confirmed_on", sa.DateTime(), nullable=True))
     op.execute("UPDATE company SET confirmed_on = '2021-12-21 01:00:00.000'")
 
 
 def downgrade():
-    op.drop_column("company", "confirmed_on")
-    op.drop_column("company", "registered_on")
+    with op.batch_alter_table("company") as batch_op:
+        batch_op.drop_column("confirmed_on")
+        batch_op.drop_column("registered_on")
