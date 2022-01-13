e9fc3f8153f8b26cb9b74b26f5577c236cbbf352
sebas <sebastian.loschert@gmx.de>
2022-01-13 14:32:57 +0100

    adapt down revision

--- migrations/versions/fb89625b2d12_add_registered_on_and_confirmed_on_to_.py
+++ migrations/versions/fb89625b2d12_add_registered_on_and_confirmed_on_to_.py
@@ -1,22 +1,19 @@
 """add registered_on and confirmed_on to comp
 
 Revision ID: fb89625b2d12
-Revises: c0dbe17daefe
+Revises: 6ad4ca971b73
 Create Date: 2022-01-11 17:15:57.832987
 
 """
 from alembic import op
 import sqlalchemy as sa
 
-## UNCOMMENT THIS AND ADAPT DOWN_REVISION
-## (HERE AND ON TOP OF FILE)
-## WHEN PR IS READY FOR MIGRATION
 
-## revision identifiers, used by Alembic.
-# revision = "fb89625b2d12"
-# down_revision = "c0dbe17daefe"
-# branch_labels = None
-# depends_on = None
+# revision identifiers, used by Alembic.
+revision = "fb89625b2d12"
+down_revision = "6ad4ca971b73"
+branch_labels = None
+depends_on = None
 
 
 def upgrade():
