d8e94f670cda1c9b20813d492d9bfbceffcc9c3a
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-01-29 17:43:11 +0100

    Refactor register member tests

--- tests/use_cases/test_register_member.py
+++ tests/use_cases/test_register_member.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 from arbeitszeit.entities import AccountTypes
 from arbeitszeit.use_cases import (
     RegisterMember,
@@ -7,7 +9,7 @@ from arbeitszeit.use_cases import (
 from tests.data_generators import MemberGenerator
 from tests.mail_service import FakeMailService
 
-from .dependency_injection import injection_test
+from .dependency_injection import get_dependency_injector
 from .repositories import AccountRepository, MemberRepository
 
 DEFAULT = dict(
@@ -20,61 +22,50 @@ DEFAULT = dict(
 )
 
 
-@injection_test
-def test_that_registering_member_is_possible(
-    use_case: RegisterMember,
-):
-    request = RegisterMemberRequest(**DEFAULT)
-    response = use_case(request)
-    assert not response.is_rejected
+class RegisterMemberTests(TestCase):
+    def setUp(self) -> None:
+        self.injector = get_dependency_injector()
+        self.use_case = self.injector.get(RegisterMember)
+        self.account_repository = self.injector.get(AccountRepository)
+        self.member_repo = self.injector.get(MemberRepository)
+        self.mail_service = self.injector.get(FakeMailService)
+        self.member_generator = self.injector.get(MemberGenerator)
 
+    def test_that_registering_member_is_possible(self):
+        request = RegisterMemberRequest(**DEFAULT)
+        response = self.use_case(request)
+        self.assertFalse(response.is_rejected)
 
-@injection_test
-def test_that_registering_a_member_does_create_a_member_account(
-    use_case: RegisterMember, account_repository: AccountRepository
-) -> None:
-    use_case(RegisterMemberRequest(**DEFAULT))
-    assert len(account_repository.accounts) == 1
-    assert account_repository.accounts[0].account_type == AccountTypes.member
+    def test_that_registering_a_member_does_create_a_member_account(self) -> None:
+        self.use_case(RegisterMemberRequest(**DEFAULT))
+        self.assertEqual(len(self.account_repository.accounts), 1)
+        self.assertEqual(
+            self.account_repository.accounts[0].account_type, AccountTypes.member
+        )
 
+    def test_that_correct_member_attributes_are_registered(self):
+        request = RegisterMemberRequest(**DEFAULT)
+        self.use_case(request)
+        assert len(self.member_repo.members) == 1
+        for member in self.member_repo.members.values():
+            self.assertEqual(member.email, request.email)
+            self.assertEqual(member.name, request.name)
+            self.assertIsNotNone(member.registered_on)
+            self.assertIsNone(member.confirmed_on)
 
-@injection_test
-def test_that_correct_member_attributes_are_registered(
-    use_case: RegisterMember,
-    member_repo: MemberRepository,
-    mail_service: FakeMailService,
-):
-    request = RegisterMemberRequest(**DEFAULT)
-    use_case(request)
-    assert len(member_repo.members) == 1
-    for member in member_repo.members.values():
-        assert member.email == request.email
-        assert member.name == request.name
-        assert member.registered_on is not None
-        assert member.confirmed_on is None
+    def test_that_mail_is_sent(self):
+        request = RegisterMemberRequest(**DEFAULT)
+        self.use_case(request)
+        self.assertEqual(len(self.mail_service.sent_mails), 1)
+        self.assertIn("Bitte bestätige dein Konto", self.mail_service.sent_mails[0])
+        self.assertIn(DEFAULT["endpoint"], self.mail_service.sent_mails[0])
 
-
-@injection_test
-def test_that_mail_is_sent(
-    use_case: RegisterMember,
-    mail_service: FakeMailService,
-):
-    request = RegisterMemberRequest(**DEFAULT)
-    use_case(request)
-    assert len(mail_service.sent_mails) == 1
-    assert "Bitte bestätige dein Konto" in mail_service.sent_mails[0]
-    assert DEFAULT["endpoint"] in mail_service.sent_mails[0]
-
-
-@injection_test
-def test_that_correct_error_is_raised_when_user_with_mail_exists(
-    use_case: RegisterMember, member_generator: MemberGenerator
-):
-    member_generator.create_member(email="test@cp.org")
-    request = RegisterMemberRequest(**DEFAULT)
-    response = use_case(request)
-    assert response.is_rejected
-    assert (
-        response.rejection_reason
-        == RegisterMemberResponse.RejectionReason.member_already_exists
-    )
+    def test_that_correct_error_is_raised_when_user_with_mail_exists(self):
+        self.member_generator.create_member(email="test@cp.org")
+        request = RegisterMemberRequest(**DEFAULT)
+        response = self.use_case(request)
+        self.assertTrue(response.is_rejected)
+        self.assertEqual(
+            response.rejection_reason,
+            RegisterMemberResponse.RejectionReason.member_already_exists,
+        )
