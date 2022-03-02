bcf1048c95d97a20339249c9bce0d169882f0109
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-03-02 23:39:02 +0100

    Implement basic docker image in nix

--- flake.nix
+++ flake.nix
@@ -12,17 +12,23 @@
           python = pkgs.python3;
         in {
           devShell = pkgs.mkShell {
-            packages = (with python.pkgs; [ black flake8 mypy isort psycopg2 ])
+            packages =
+              (with python.pkgs; [ black flake8 mypy isort psycopg2 gunicorn ])
               ++ (with pkgs; [ nixfmt ]);
             inputsFrom = [ python.pkgs.arbeitszeitapp ];
           };
           defaultPackage = pkgs.python3.pkgs.arbeitszeitapp;
+          packages = {
+            arbeitszeitapp-docker-image = pkgs.arbeitszeitapp-docker-image;
+          };
         });
       systemIndependent = {
         overlay = final: prev: {
           python3 = prev.python3.override {
             packageOverrides = import nix/pythonPackages.nix;
           };
+          arbeitszeitapp-docker-image =
+            prev.callPackage nix/docker.nix { python = final.python3; };
         };
       };
     in systemDependent // systemIndependent;
