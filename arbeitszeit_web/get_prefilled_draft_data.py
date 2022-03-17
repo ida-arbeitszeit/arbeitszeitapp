13bd7b5f35fab453f64c6ad685566e8716f9d45c
sebas <sebastian.loschert@gmx.de>
2022-03-17 22:04:10 +0100

    add assertion to controller

--- arbeitszeit_web/get_prefilled_draft_data.py
+++ arbeitszeit_web/get_prefilled_draft_data.py
@@ -44,17 +44,27 @@ class PrefilledDraftDataController:
     def import_form_data(
         self, planner: UUID, draft_form: CreateDraftForm
     ) -> CreatePlanDraftRequest:
+        labour_costs = Decimal(draft_form.get_costs_a_string())
+        resource_cost = Decimal(draft_form.get_costs_r_string())
+        means_cost = Decimal(draft_form.get_costs_p_string())
+        production_amount = int(draft_form.get_prd_amount_string())
+        timeframe_in_days = int(draft_form.get_timeframe_string())
+        assert labour_costs >= 0
+        assert resource_cost >= 0
+        assert means_cost >= 0
+        assert production_amount >= 0
+        assert timeframe_in_days >= 0
         return CreatePlanDraftRequest(
             costs=ProductionCosts(
-                labour_cost=Decimal(draft_form.get_costs_a_string()),
-                resource_cost=Decimal(draft_form.get_costs_r_string()),
-                means_cost=Decimal(draft_form.get_costs_p_string()),
+                labour_cost=labour_costs,
+                resource_cost=resource_cost,
+                means_cost=means_cost,
             ),
             product_name=draft_form.get_prd_name_string(),
             production_unit=draft_form.get_prd_unit_string(),
-            production_amount=int(draft_form.get_prd_amount_string()),
+            production_amount=production_amount,
             description=draft_form.get_description_string(),
-            timeframe_in_days=int(draft_form.get_timeframe_string()),
+            timeframe_in_days=timeframe_in_days,
             is_public_service=True
             if draft_form.get_productive_or_public_string() == "public"
             else False,
