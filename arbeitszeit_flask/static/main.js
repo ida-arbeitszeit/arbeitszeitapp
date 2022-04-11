824e0d5ca8a0cfb5b235e835646276189b23e6fe
sebas <sebastian.loschert@gmx.de>
2022-04-11 16:43:39 +0200

    prettify show_notifications & enable close of box

--- arbeitszeit_flask/static/main.js
+++ arbeitszeit_flask/static/main.js
@@ -2,3 +2,14 @@ function expandMenu() {
     var element = document.getElementById("navbarOnTop");
     element.classList.toggle("is-active");
 }
+
+// close notification box
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+});
