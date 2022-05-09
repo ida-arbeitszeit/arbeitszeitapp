69dfbc3fd5c7288d243e3cd2816dce93ddae77ec
sebas <sebastian.loschert@gmx.de>
2022-05-09 23:42:22 +0200

    delete deprecated extensions from jinja

--- tests/flask_integration/conftest.py
+++ tests/flask_integration/conftest.py
@@ -9,13 +9,5 @@ def install_warning_filters():
         "ignore",
         message=r"Dialect sqlite\+pysqlite does \*not\* support Decimal objects natively, and SQLAlchemy must convert from floating point - rounding errors and other issues may occur",
     )
-    warnings.filterwarnings(
-        "ignore",
-        message=r"The 'autoescape' extension is deprecated and will be removed in Jinja 3.1",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        message=r"The 'with' extension is deprecated and will be removed in Jinja 3.1",
-    )
     yield
     warnings.resetwarnings()
