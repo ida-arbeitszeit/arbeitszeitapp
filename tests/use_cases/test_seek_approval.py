44f9b607539adfb4c3d69343b9812f211d405415
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-05-24 21:43:29 +0200

    Refactor test_seek_approval to use class based tests

--- tests/use_cases/test_seek_approval.py
+++ tests/use_cases/test_seek_approval.py
@@ -1,80 +1,63 @@
 from datetime import datetime
+from unittest import TestCase
 from uuid import UUID
 
-from arbeitszeit.repositories import PlanDraftRepository, PlanRepository
 from arbeitszeit.use_cases import SeekApproval
 from tests.data_generators import PlanGenerator
 from tests.datetime_service import FakeDatetimeService
 
-from .dependency_injection import injection_test
+from .dependency_injection import get_dependency_injector
+from .repositories import PlanDraftRepository, PlanRepository
 
 
-@injection_test
-def test_that_any_plan_will_be_approved(
-    plan_generator: PlanGenerator,
-    seek_approval: SeekApproval,
-):
-    plan_draft = plan_generator.draft_plan()
-    approval_response = seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
-    assert approval_response.is_approved
+class UseCaseTests(TestCase):
+    def setUp(self) -> None:
+        self.injector = get_dependency_injector()
+        self.seek_approval = self.injector.get(SeekApproval)
+        self.plan_generator = self.injector.get(PlanGenerator)
+        self.draft_repository = self.injector.get(PlanDraftRepository)
+        self.plan_repository = self.injector.get(PlanRepository)
+        self.datetime_service = self.injector.get(FakeDatetimeService)
 
+    def test_that_any_plan_will_be_approved(self) -> None:
+        plan_draft = self.plan_generator.draft_plan()
+        approval_response = self.seek_approval(
+            SeekApproval.Request(draft_id=plan_draft.id)
+        )
+        assert approval_response.is_approved
 
-@injection_test
-def test_plan_draft_gets_deleted_after_approval(
-    plan_generator: PlanGenerator,
-    seek_approval: SeekApproval,
-    draft_repository: PlanDraftRepository,
-):
-    plan_draft = plan_generator.draft_plan()
-    response = seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
-    assert response.is_approved
-    assert draft_repository.get_by_id(plan_draft.id) is None
+    def test_plan_draft_gets_deleted_after_approval(self) -> None:
+        plan_draft = self.plan_generator.draft_plan()
+        response = self.seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
+        assert response.is_approved
+        assert self.draft_repository.get_by_id(plan_draft.id) is None
 
+    def test_that_true_is_returned(self) -> None:
+        plan_draft = self.plan_generator.draft_plan()
+        approval_response = self.seek_approval(
+            SeekApproval.Request(draft_id=plan_draft.id)
+        )
+        assert approval_response.is_approved is True
 
-@injection_test
-def test_that_true_is_returned(
-    plan_generator: PlanGenerator,
-    seek_approval: SeekApproval,
-):
-    plan_draft = plan_generator.draft_plan()
-    approval_response = seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
-    assert approval_response.is_approved is True
+    def test_that_returned_new_plan_id_is_uuid(self) -> None:
+        plan_draft = self.plan_generator.draft_plan()
+        approval_response = self.seek_approval(
+            SeekApproval.Request(draft_id=plan_draft.id)
+        )
+        assert isinstance(approval_response.new_plan_id, UUID)
 
+    def test_that_approval_date_has_correct_day_of_month(self) -> None:
+        self.datetime_service.freeze_time(datetime(year=2021, month=5, day=3))
+        plan_draft = self.plan_generator.draft_plan()
+        self.seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
+        new_plan = self.plan_repository.get_plan_by_id(plan_draft.id)
+        assert new_plan
+        assert new_plan.approval_date
+        assert 3 == new_plan.approval_date.day
 
-@injection_test
-def test_that_returned_new_plan_id_is_uuid(
-    plan_generator: PlanGenerator,
-    seek_approval: SeekApproval,
-):
-    plan_draft = plan_generator.draft_plan()
-    approval_response = seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
-    assert isinstance(approval_response.new_plan_id, UUID)
-
-
-@injection_test
-def test_that_approval_date_has_correct_day_of_month(
-    plan_generator: PlanGenerator,
-    seek_approval: SeekApproval,
-    datetime_service: FakeDatetimeService,
-    plan_repository: PlanRepository,
-):
-    datetime_service.freeze_time(datetime(year=2021, month=5, day=3))
-    plan_draft = plan_generator.draft_plan()
-    seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
-    new_plan = plan_repository.get_plan_by_id(plan_draft.id)
-    assert new_plan
-    assert new_plan.approval_date
-    assert 3 == new_plan.approval_date.day
-
-
-@injection_test
-def test_that_approved_plan_has_same_planner_as_draft(
-    plan_generator: PlanGenerator,
-    seek_approval: SeekApproval,
-    plan_repository: PlanRepository,
-):
-    plan_draft = plan_generator.draft_plan()
-    seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
-    new_plan = plan_repository.get_plan_by_id(plan_draft.id)
-    assert new_plan
-    assert new_plan.planner == plan_draft.planner
+    def test_that_approved_plan_has_same_planner_as_draft(self) -> None:
+        plan_draft = self.plan_generator.draft_plan()
+        self.seek_approval(SeekApproval.Request(draft_id=plan_draft.id))
+        new_plan = self.plan_repository.get_plan_by_id(plan_draft.id)
+        assert new_plan
+        assert new_plan.planner == plan_draft.planner
