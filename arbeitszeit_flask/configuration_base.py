900ec34238a1f6e6b2204422d15d666ca0886e6c
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-05-25 13:11:48 +0200

    Allow configuration of https policy

--- arbeitszeit_flask/configuration_base.py
+++ arbeitszeit_flask/configuration_base.py
@@ -8,3 +8,4 @@ SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/arbeitszeitapp.db"
 SECURITY_PASSWORD_SALT = environ.get("SECURITY_PASSWORD_SALT")
 LANGUAGES = {"en": "English", "de": "Deutsch"}
 MAIL_PORT = "25"
+FORCE_HTTPS = True
