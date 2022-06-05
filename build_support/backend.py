2e1b93ec9daf6839a96c6da0cdf5671caafd70a0
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-06-05 16:13:53 +0200

    Compile translations when build wheels

---
+++ build_support/backend.py
@@ -0,0 +1,23 @@
+from setuptools import build_meta as _orig
+
+
+def prepare_metadata_for_build_wheel(metadata_directory, config_settings=None):
+    import subprocess
+    subprocess.run(["python", "setup.py", "compile_catalog"])
+    return _orig.prepare_metadata_for_build_wheel(metadata_directory, config_settings=None)
+
+
+def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
+    return _orig.build_wheel(wheel_directory, config_settings, metadata_directory)
+
+
+def build_sdist(sdist_directory, config_settings=None):
+    return _orig.build_sdist(sdist_directory, config_settings)
+
+
+def get_requires_for_build_wheel(self, config_settings=None):
+    return _orig.get_requires_for_build_wheel(config_settings)
+
+
+def get_requires_for_build_sdist(self, config_settings=None):
+    return _orig.get_requires_for_build_sdist(config_settings) + ["flask-babel"]
