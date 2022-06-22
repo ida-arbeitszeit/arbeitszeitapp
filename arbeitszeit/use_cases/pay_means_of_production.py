7b6d53e45ec3408e83bcba45829b4e48d8aca4e1
sebas <sebastian.loschert@gmx.de>
2022-06-22 22:14:07 +0200

    fixing bug in pay_means_of_production

--- arbeitszeit/use_cases/pay_means_of_production.py
+++ arbeitszeit/use_cases/pay_means_of_production.py
@@ -8,7 +8,7 @@ from uuid import UUID
 from injector import inject
 
 from arbeitszeit.datetime_service import DatetimeService
-from arbeitszeit.entities import Company, Plan, PurposesOfPurchases
+from arbeitszeit.entities import Company, Member, Plan, PurposesOfPurchases
 from arbeitszeit.price_calculator import calculate_price
 from arbeitszeit.repositories import (
     CompanyRepository,
@@ -121,7 +121,7 @@ class Payment:
             purchase_date=self.datetime_service.now(),
             plan=self.plan.id,
             buyer=self.buyer.id,
-            is_member=isinstance(self.buyer, Company),
+            is_member=isinstance(self.buyer, Member),
             price_per_unit=price_per_unit,
             amount=self.amount,
             purpose=self.purpose,
