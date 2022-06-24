6d9972af516dc0fdeb95ec2e90e0c89cee56b2c1
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-06-24 16:38:43 +0200

    Use proper mccabe version for building flake8

--- nix/developmentOverrides.nix
+++ nix/developmentOverrides.nix
@@ -5,4 +5,15 @@ self: super: {
         --replace "rv = [sys.executable]" "return sys.argv"
     '';
   });
+  mccabe = super.mccabe.overrideAttrs( old: {
+    src = super.fetchPypi {
+      pname = "mccabe";
+      version = "0.6.1";
+      sha256 = "3Y0YIoWg/la6zn9FtefRpuvL9STo872H6w8SUnG4gx8=";
+    };
+    buildInputs = old.buildInputs ++ [self.pytest-runner];
+  });
+  flake8 = super.flake8.overrideAttrs( old: {
+    postPatch = "";
+  });
 }
