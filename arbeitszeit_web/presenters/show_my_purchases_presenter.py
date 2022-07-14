4aa8e2019e255c5224b3d458981577bcf1a0df08
Philip <philip.broistedt@gmx.de>
2022-07-14 22:21:29 +0200

    NEW: add ouout fomating to ShowMyPurchasesPresenter

--- arbeitszeit_web/presenters/show_my_purchases_presenter.py
+++ arbeitszeit_web/presenters/show_my_purchases_presenter.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
+from arbeitszeit_flask.datetime import RealtimeDatetimeService
 from decimal import Decimal
 from arbeitszeit.use_cases.query_purchases import PurchaseQueryResponse
 from typing import Iterator
+
 
 @dataclass
 class ViewModel:
@@ -11,24 +13,26 @@ class ViewModel:
         purchase_date: datetime
         product_name: str
         product_description: str
-        price_per_unit: Decimal
+        price_per_unit: str
         amount: int
-        price_total: Decimal
+        price_total: str
     
-    purchases: list
-
+    purchases: list[Purchase] = field(default_factory=list)
+    
     def append(self, purchase_respond: PurchaseQueryResponse):
+        
         p = self.Purchase(
-            purchase_date=purchase_respond.purchase_date,  # format_datetime(zone='Europe/Berlin', fmt='%d.%m.%Y')
+            #purchase_date=purchase_respond.purchase_date,  # format_datetime(zone='Europe/Berlin', fmt='%d.%m.%Y')
+            purchase_date=RealtimeDatetimeService().
+                    format_datetime(date=purchase_respond.purchase_date),
             product_name=purchase_respond.product_name,
             product_description=purchase_respond.product_description,
-            price_per_unit=purchase_respond.price_per_unit,
+            price_per_unit=round(purchase_respond.price_per_unit, 2),
             amount=purchase_respond.amount,
-            price_total=purchase_respond.price_total,
+            price_total=round(purchase_respond.price_total, 2),
         )
         self.purchases.append(p)
 
-    # us from typing import List ?? see ShowPAccountDetailsPresenter
     def __len__(self):
         return len(self.purchases)
 
@@ -40,19 +44,10 @@ class ViewModel:
 class ShowMyPurchasesPresenter:
     def present(self, use_case_response: Iterator[PurchaseQueryResponse]) -> ViewModel:
         
-        model = ViewModel([])
+        model = ViewModel()
 
         for p in use_case_response:
             model.append(p)
 
         return model
-        """
-        return ViewModel(
-            purchase_date=use_case_response.purchase_date,
-            product_name=use_case_response.product_name,
-            product_description=use_case_response.product_description,
-            price_per_unit=use_case_response.price_per_unit,
-            amount=use_case_response.amount,
-            price_total=use_case_response.price_total,
-        )
-        """
+
