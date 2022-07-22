e3bebf70b203b4f8cf007c0cb2c79a09661dc2ad
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-07-22 18:59:05 +0200

    Auto-update api documentation

--- nix/pythonPackages/arbeitszeitapp.nix
+++ nix/pythonPackages/arbeitszeitapp.nix
@@ -2,11 +2,14 @@
 
 # python packages
 , email_validator, flask, flask-babel, flask-talisman, flask_login, flask_mail
-, flask_migrate, flask_wtf, hypothesis, injector, is_safe_url, matplotlib }:
+, flask_migrate, flask_wtf, hypothesis, injector, is_safe_url, matplotlib
+, sphinx }:
 buildPythonPackage {
   pname = "arbeitszeitapp";
   version = "develop";
   src = ../..;
+  outputs = [ "out" "docs" ];
+  postPhases = [ "buildDocsPhase" ];
   format = "pyproject";
   buildInputs = [ pytestCheckHook ];
   checkInputs = [ hypothesis ];
@@ -23,4 +26,7 @@ buildPythonPackage {
     is_safe_url
     matplotlib
   ];
+  buildDocsPhase = ''
+    ${sphinx}/bin/sphinx-build -a $src/docs $docs
+  '';
 }
