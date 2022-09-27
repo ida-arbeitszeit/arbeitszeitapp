5c968991dc47502b0a787b0a14ac7272d7e43c64
sebas <sebastian.loschert@gmx.de>
2022-09-27 22:11:01 +0200

    integration tests for company and member dashboard

--- tests/flask_integration/test_member_dashboard_view.py
+++ tests/flask_integration/test_member_dashboard_view.py
@@ -4,7 +4,7 @@ from tests.flask_integration.flask import ViewTestCase
 class AnonymousUserTest(ViewTestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.url = "/company/dashboard"
+        self.url = "/member/dashboard"
 
     def test_anonymous_user_gets_302(
         self,
@@ -16,47 +16,47 @@ class AnonymousUserTest(ViewTestCase):
         self,
     ):
         response = self.client.get(self.url)
-        self.assertEqual(response.location, "/?next=%2Fcompany%2Fdashboard")
+        self.assertEqual(response.location, "/?next=%2Fmember%2Fdashboard")
 
 
-class MemberTest(ViewTestCase):
+class CompanyTest(ViewTestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.url = "/company/dashboard"
-        self.member = self.login_member(confirm_member=True)
+        self.url = "/member/dashboard"
+        self.company = self.login_company(confirm_company=True)
 
-    def test_member_gets_302(self) -> None:
+    def test_company_gets_302(self) -> None:
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
 
-    def test_member_gets_redirected_to_start_page_with_next_url_set_correctly(
+    def test_company_gets_redirected_to_start_page_with_next_url_set_correctly(
         self,
     ) -> None:
         response = self.client.get(self.url)
-        self.assertEqual(response.location, "/?next=%2Fcompany%2Fdashboard")
+        self.assertEqual(response.location, "/?next=%2Fmember%2Fdashboard")
 
 
-class UnconfirmedCompanyTests(ViewTestCase):
+class UnconfirmedMemberTests(ViewTestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.company = self.login_company(confirm_company=False)
-        self.url = "/company/dashboard"
+        self.member = self.login_member(confirm_member=False)
+        self.url = "/member/dashboard"
 
-    def test_unconfirmed_company_gets_302(self) -> None:
+    def test_unconfirmed_member_gets_302(self) -> None:
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
 
-    def test_redirects_to_page_for_unconfirmed_companies(self) -> None:
+    def test_redirects_to_page_for_unconfirmed_members(self) -> None:
         response = self.client.get(self.url)
-        self.assertEqual(response.location, "/company/unconfirmed")
+        self.assertEqual(response.location, "/member/unconfirmed")
 
 
-class ConfirmedCompanyTests(ViewTestCase):
+class ConfirmedMemberTests(ViewTestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.company = self.login_company(confirm_company=True)
-        self.url = "/company/dashboard"
+        self.member = self.login_member(confirm_member=True)
+        self.url = "/member/dashboard"
 
-    def test_company_gets_200(self) -> None:
+    def test_member_gets_200(self) -> None:
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
