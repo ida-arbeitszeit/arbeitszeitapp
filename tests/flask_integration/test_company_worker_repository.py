eab4377c3aaa4eea7c84cc31495e3f1da7b5a37b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-09-30 00:15:09 +0200

    arbeitszeit.repositories: Refactor CompanyWorkerRepository

--- tests/flask_integration/test_company_worker_repository.py
+++ tests/flask_integration/test_company_worker_repository.py
@@ -1,60 +1,39 @@
 from uuid import uuid4
 
-from arbeitszeit_flask.database.repositories import (
-    CompanyWorkerRepository,
-    MemberRepository,
-)
+from arbeitszeit_flask.database.repositories import CompanyWorkerRepository
 from tests.data_generators import CompanyGenerator, MemberGenerator
-from tests.flask_integration.dependency_injection import injection_test
+
+from .flask import FlaskTestCase
 
 
-@injection_test
-def test_that_no_workplaces_are_returned_for_new_member_account(
-    member_generator: MemberGenerator,
-    repo: CompanyWorkerRepository,
-):
-    member = member_generator.create_member()
-    workplaces = repo.get_member_workplaces(member.id)
-    assert not workplaces
+class RepositoryTester(FlaskTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.member_generator: MemberGenerator = self.injector.get(MemberGenerator)
+        self.repo: CompanyWorkerRepository = self.injector.get(CompanyWorkerRepository)
+        self.company_generator: CompanyGenerator = self.injector.get(CompanyGenerator)
 
+    def test_that_no_workplaces_are_returned_for_new_member_account(self) -> None:
+        member = self.member_generator.create_member()
+        workplaces = self.repo.get_member_workplaces(member.id)
+        assert not workplaces
 
-@injection_test
-def test_that_no_workplaces_are_returned_non_existing_worker(
-    repo: CompanyWorkerRepository,
-):
-    workplaces = repo.get_member_workplaces(uuid4())
-    assert not workplaces
+    def test_that_no_workplaces_are_returned_non_existing_worker(self) -> None:
+        workplaces = self.repo.get_member_workplaces(uuid4())
+        assert not workplaces
 
+    def test_that_workplace_is_returned_after_one_is_registered(self) -> None:
+        member = self.member_generator.create_member()
+        company = self.company_generator.create_company()
+        self.repo.add_worker_to_company(company=company.id, worker=member.id)
+        assert [company] == self.repo.get_member_workplaces(member.id)
 
-@injection_test
-def test_that_workplace_is_returned_after_one_is_registered(
-    member_generator: MemberGenerator,
-    member_repository: MemberRepository,
-    company_generator: CompanyGenerator,
-    repo: CompanyWorkerRepository,
-):
-    member = member_generator.create_member()
-    company = company_generator.create_company()
-    repo.add_worker_to_company(company=company, worker=member)
-    assert [company] == repo.get_member_workplaces(member.id)
+    def test_no_company_workers_are_returned_for_fresh_company(self) -> None:
+        company = self.company_generator.create_company()
+        assert not self.repo.get_company_workers(company.id)
 
-
-@injection_test
-def test_no_company_workers_are_returned_for_fresh_company(
-    company_generator: CompanyGenerator,
-    repo: CompanyWorkerRepository,
-):
-    company = company_generator.create_company()
-    assert not repo.get_company_workers(company)
-
-
-@injection_test
-def test_worker_that_was_added_shows_up_in_company_workers(
-    company_generator: CompanyGenerator,
-    member_generator: MemberGenerator,
-    repo: CompanyWorkerRepository,
-):
-    company = company_generator.create_company()
-    member = member_generator.create_member()
-    repo.add_worker_to_company(company=company, worker=member)
-    assert member in repo.get_company_workers(company)
+    def test_worker_that_was_added_shows_up_in_company_workers(self) -> None:
+        company = self.company_generator.create_company()
+        member = self.member_generator.create_member()
+        self.repo.add_worker_to_company(company=company.id, worker=member.id)
+        assert member in self.repo.get_company_workers(company.id)
