fab3e4c5e9c330241e93f009563aaefc36d4a9e8
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-11-02 15:38:46 +0100

    Add type annotation to accountant/routes.py

--- arbeitszeit_flask/accountant/routes.py
+++ arbeitszeit_flask/accountant/routes.py
@@ -1,6 +1,5 @@
-from flask import Response
-
 from arbeitszeit_flask.template import UserTemplateRenderer
+from arbeitszeit_flask.types import Response
 from arbeitszeit_web.presenters.get_accountant_dashboard_presenter import (
     GetAccountantDashboardPresenter,
 )
@@ -11,7 +10,7 @@ from .blueprint import AccountantRoute
 @AccountantRoute("/accountant/dashboard")
 def dashboard(
     template_renderer: UserTemplateRenderer, presenter: GetAccountantDashboardPresenter
-):
+) -> Response:
     view_model = presenter.create_dashboard_view_model()
     return template_renderer.render_template(
         "accountant/dashboard.html",
