7c8fc787205c8958e626c8ed9aa66c4a5096bde4
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-11-04 00:22:55 +0100

    Rename test

--- tests/flask_integration/test_approve_plan_view.py
+++ tests/flask_integration/test_approve_plan_view.py
@@ -9,7 +9,7 @@ class AccountantTests(ViewTestCase):
         self.plan_generator = self.injector.get(PlanGenerator)
         self.login_accountant()
 
-    def test(self) -> None:
+    def test_that_user_gets_redirected(self) -> None:
         plan = self.plan_generator.create_plan()
         response = self.client.post(f"/accountant/plans/{plan.id}/approve")
         assert response.status_code == 302
