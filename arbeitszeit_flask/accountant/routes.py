0330ccb22e88a8d9bbe57cf707c761bde8b9e216
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-11-04 00:19:07 +0100

    Implement route to approve plans as accountant

--- arbeitszeit_flask/accountant/routes.py
+++ arbeitszeit_flask/accountant/routes.py
@@ -1,8 +1,16 @@
+from uuid import UUID
+
+from flask import redirect
+
+from arbeitszeit.use_cases.approve_plan import ApprovePlanUseCase
 from arbeitszeit.use_cases.list_plans_with_pending_review import (
     ListPlansWithPendingReviewUseCase,
 )
+from arbeitszeit_flask.database import commit_changes
 from arbeitszeit_flask.template import UserTemplateRenderer
 from arbeitszeit_flask.types import Response
+from arbeitszeit_web.controllers.approve_plan_controller import ApprovePlanController
+from arbeitszeit_web.presenters.approve_plan_presenter import ApprovePlanPresenter
 from arbeitszeit_web.presenters.get_accountant_dashboard_presenter import (
     GetAccountantDashboardPresenter,
 )
@@ -37,3 +45,17 @@ def list_plans_with_pending_review(
         "accountant/plans-to-review-list.html",
         context=dict(view_model=view_model),
     )
+
+
+@AccountantRoute("/accountant/plans/<uuid:plan>/approve", methods=["POST"])
+@commit_changes
+def approve_plan(
+    plan: UUID,
+    controller: ApprovePlanController,
+    use_case: ApprovePlanUseCase,
+    presenter: ApprovePlanPresenter,
+) -> Response:
+    request = controller.approve_plan(plan)
+    response = use_case.approve_plan(request)
+    view_model = presenter.approve_plan(response)
+    return redirect(view_model.redirect_url)
---
+++ arbeitszeit_web/presenters/approve_plan_presenter.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from injector import inject
+
+from arbeitszeit.use_cases.approve_plan import ApprovePlanUseCase as UseCase
+from arbeitszeit_web.notification import Notifier
+from arbeitszeit_web.translator import Translator
+from arbeitszeit_web.url_index import UrlIndex
+
+
+@inject
+@dataclass
+class ApprovePlanPresenter:
+    @dataclass
+    class ViewModel:
+        redirect_url: str
+
+    url_index: UrlIndex
+    notifier: Notifier
+    translator: Translator
+
+    def approve_plan(self, response: UseCase.Response) -> ViewModel:
+        if response.is_approved:
+            self.notifier.display_info(
+                self.translator.gettext("Plan was approved successfully")
+            )
+        else:
+            self.notifier.display_warning(
+                self.translator.gettext("Plan approval failed")
+            )
+        return self.ViewModel(
+            redirect_url=self.url_index.get_unreviewed_plans_list_view_url()
+        )
