bb4411884471901b094119fd057cd9b629e5177d
sebas <sebastian.loschert@gmx.de>
2023-01-15 22:11:41 +0100

    adjust test names

--- tests/api/presenters/test_list_active_plans_presenter.py
+++ tests/api/presenters/test_list_active_plans_presenter.py
@@ -10,18 +10,18 @@ class TestGetPresenter(BaseTestCase):
         self.queried_plan_generator = self.injector.get(QueriedPlanGenerator)
         self.presenter = ActivePlansPresenter()
 
-    def test_top_level(self) -> None:
+    def test_schema_top_level(self) -> None:
         schema = self.presenter.get_schema()
         assert isinstance(schema, JsonDict)
         assert schema.as_list
         assert schema.schema_name == "PlanList"
 
-    def test_top_level_members(self) -> None:
+    def test_schema_top_level_members(self) -> None:
         schema = self.presenter.get_schema()
         assert isinstance(schema, JsonDict)
         assert isinstance(schema.members["results"], JsonDict)
 
-    def test_second_level(self) -> None:
+    def test_schema_second_level(self) -> None:
         top_schema = self.presenter.get_schema()
         assert isinstance(top_schema, JsonDict)
         schema = top_schema.members["results"]
@@ -30,7 +30,7 @@ class TestGetPresenter(BaseTestCase):
         assert not schema.as_list
         assert schema.schema_name == "Plan"
 
-    def test_second_level_members(self) -> None:
+    def test_schema_second_level_members(self) -> None:
         top_schema = self.presenter.get_schema()
         assert isinstance(top_schema, JsonDict)
         schema = top_schema.members["results"]
