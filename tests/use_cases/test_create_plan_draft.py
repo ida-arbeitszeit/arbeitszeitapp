e9a2e8beaf10c57d09a59db828103c400c6981c2
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-01-19 07:48:22 +0100

    Refactor test_create_plan_draft.py

--- tests/use_cases/test_create_plan_draft.py
+++ tests/use_cases/test_create_plan_draft.py
@@ -5,9 +5,8 @@ from uuid import uuid4
 from arbeitszeit.entities import ProductionCosts
 from arbeitszeit.use_cases import CreatePlanDraft, CreatePlanDraftRequest
 from arbeitszeit.use_cases.create_plan_draft import CreatePlanDraftResponse
-from tests.data_generators import CompanyGenerator, PlanGenerator
 
-from .dependency_injection import injection_test
+from .base_test_case import BaseTestCase
 from .repositories import PlanDraftRepository
 
 REQUEST = CreatePlanDraftRequest(
@@ -26,105 +25,73 @@ REQUEST = CreatePlanDraftRequest(
 )
 
 
-@injection_test
-def test_that_create_plan_creates_a_plan_draft_that_is_not_rejected(
-    plan_draft_repository: PlanDraftRepository,
-    create_plan_draft: CreatePlanDraft,
-    company_generator: CompanyGenerator,
-):
-    planner = company_generator.create_company_entity()
-    request = replace(REQUEST, planner=planner.id)
-    assert not len(plan_draft_repository)
-    response = create_plan_draft(request)
-    assert len(plan_draft_repository) == 1
-    assert not response.is_rejected
+class UseCaseTests(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.create_plan_draft = self.injector.get(CreatePlanDraft)
+        self.plan_draft_repository = self.injector.get(PlanDraftRepository)
 
+    def test_that_create_plan_creates_a_plan_draft_that_is_not_rejected(self) -> None:
+        planner = self.company_generator.create_company()
+        request = replace(REQUEST, planner=planner)
+        assert not len(self.plan_draft_repository)
+        response = self.create_plan_draft(request)
+        assert len(self.plan_draft_repository) == 1
+        assert not response.is_rejected
 
-@injection_test
-def test_that_create_plan_returns_a_draft_id(
-    create_plan_draft: CreatePlanDraft,
-    company_generator: CompanyGenerator,
-):
-    planner = company_generator.create_company_entity()
-    request = replace(REQUEST, planner=planner.id)
-    response = create_plan_draft(request)
-    assert response.draft_id
+    def test_that_create_plan_returns_a_draft_id(self) -> None:
+        planner = self.company_generator.create_company()
+        request = replace(REQUEST, planner=planner)
+        response = self.create_plan_draft(request)
+        assert response.draft_id
 
+    def test_that_create_plan_gets_rejected_with_non_existing_planner(self) -> None:
+        request = replace(
+            REQUEST,
+            planner=uuid4(),
+        )
+        response = self.create_plan_draft(request)
+        assert response.is_rejected
+        assert (
+            response.rejection_reason
+            == CreatePlanDraftResponse.RejectionReason.planner_does_not_exist
+        )
+        assert not response.draft_id
 
-@injection_test
-def test_that_create_plan_gets_rejected_with_non_existing_planner(
-    create_plan_draft: CreatePlanDraft,
-):
-    request = replace(
-        REQUEST,
-        planner=uuid4(),
-    )
-    response = create_plan_draft(request)
-    assert response.is_rejected
-    assert (
-        response.rejection_reason
-        == CreatePlanDraftResponse.RejectionReason.planner_does_not_exist
-    )
-    assert not response.draft_id
+    def test_that_create_plan_gets_rejected_with_negative_production_costs(
+        self,
+    ) -> None:
+        planner = self.company_generator.create_company()
+        request = replace(
+            REQUEST,
+            planner=planner,
+            costs=ProductionCosts(Decimal(-1), Decimal(-1), Decimal(-1)),
+        )
+        response = self.create_plan_draft(request)
+        assert response.is_rejected
+        assert (
+            response.rejection_reason
+            == CreatePlanDraftResponse.RejectionReason.negative_plan_input
+        )
+        assert not response.draft_id
 
+    def test_that_create_plan_gets_rejected_with_negative_production_amount(
+        self,
+    ) -> None:
+        planner = self.company_generator.create_company()
+        request = replace(REQUEST, planner=planner, production_amount=-1)
+        response = self.create_plan_draft(request)
+        assert response.is_rejected
+        assert not response.draft_id
 
-@injection_test
-def test_that_create_plan_gets_rejected_with_negative_production_costs(
-    create_plan_draft: CreatePlanDraft,
-    company_generator: CompanyGenerator,
-):
-    planner = company_generator.create_company_entity()
-    request = replace(
-        REQUEST,
-        planner=planner.id,
-        costs=ProductionCosts(Decimal(-1), Decimal(-1), Decimal(-1)),
-    )
-    response = create_plan_draft(request)
-    assert response.is_rejected
-    assert (
-        response.rejection_reason
-        == CreatePlanDraftResponse.RejectionReason.negative_plan_input
-    )
-    assert not response.draft_id
+    def test_that_create_plan_gets_rejected_with_negative_timeframe(self) -> None:
+        planner = self.company_generator.create_company()
+        request = replace(REQUEST, planner=planner, timeframe_in_days=-1)
+        response = self.create_plan_draft(request)
+        assert response.is_rejected
+        assert not response.draft_id
 
-
-@injection_test
-def test_that_create_plan_gets_rejected_with_negative_production_amount(
-    create_plan_draft: CreatePlanDraft,
-    company_generator: CompanyGenerator,
-):
-    planner = company_generator.create_company_entity()
-    request = replace(
-        REQUEST,
-        planner=planner.id,
-        production_amount=-1,
-    )
-    response = create_plan_draft(request)
-    assert response.is_rejected
-    assert not response.draft_id
-
-
-@injection_test
-def test_that_create_plan_gets_rejected_with_negative_timeframe(
-    create_plan_draft: CreatePlanDraft,
-    company_generator: CompanyGenerator,
-):
-    planner = company_generator.create_company_entity()
-    request = replace(
-        REQUEST,
-        planner=planner.id,
-        timeframe_in_days=-1,
-    )
-    response = create_plan_draft(request)
-    assert response.is_rejected
-    assert not response.draft_id
-
-
-@injection_test
-def test_that_drafted_plan_has_same_planner_as_specified_on_creation(
-    plan_generator: PlanGenerator,
-    company_generator: CompanyGenerator,
-) -> None:
-    planner = company_generator.create_company()
-    draft = plan_generator.draft_plan(planner=planner)
-    assert draft.planner.id == planner
+    def test_that_drafted_plan_has_same_planner_as_specified_on_creation(self) -> None:
+        planner = self.company_generator.create_company()
+        draft = self.plan_generator.draft_plan(planner=planner)
+        assert draft.planner.id == planner
