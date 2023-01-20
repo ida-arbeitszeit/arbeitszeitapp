ec844d22bb9fa69d3418d26e34664a9930116111
sebas <sebastian.loschert@gmx.de>
2023-01-20 17:17:54 +0100

    api presenter test improvement

--- tests/api/presenters/test_list_active_plans_presenter.py
+++ tests/api/presenters/test_list_active_plans_presenter.py
@@ -36,26 +36,26 @@ class TestGetPresenter(BaseTestCase):
         assert not schema.as_list
         assert schema.schema_name == "Plan"
 
-    def test_schema_second_level_members(self) -> None:
+    def test_schema_second_level_members_field_types_are_correct(self) -> None:
         top_schema = self.presenter.get_schema()
         assert isinstance(top_schema, JsonDict)
-        schema = top_schema.members["results"]
-        assert isinstance(schema, JsonDict)
+        second_level_schema = top_schema.members["results"]
+        assert isinstance(second_level_schema, JsonDict)
 
-        assert len(schema.members) == 10
+        field_expectations = [
+            ("plan_id", JsonString),
+            ("company_name", JsonString),
+            ("company_id", JsonString),
+            ("product_name", JsonString),
+            ("description", JsonString),
+            ("is_public_service", JsonBoolean),
+            ("is_available", JsonBoolean),
+            ("is_cooperating", JsonBoolean),
+            ("price_per_unit", JsonDecimal),
+            ("activation_date", JsonDatetime),
+        ]
 
-        for name in [
-            "plan_id",
-            "company_name",
-            "company_id",
-            "product_name",
-            "description",
-        ]:
-            assert isinstance(schema.members[name], JsonString)
+        assert len(second_level_schema.members) == len(field_expectations)
 
-        for name in ["is_public_service", "is_available", "is_cooperating"]:
-            assert isinstance(schema.members[name], JsonBoolean)
-
-        assert isinstance(schema.members["price_per_unit"], JsonDecimal)
-
-        assert isinstance(schema.members["activation_date"], JsonDatetime)
+        for field_name, expected_type in field_expectations:
+            assert isinstance(second_level_schema.members[field_name], expected_type)
