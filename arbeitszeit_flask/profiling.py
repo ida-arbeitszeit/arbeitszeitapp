875760d6fd533259f8fe5dba4f36936092ca8fa3
sebas <sebastian.loschert@gmx.de>
2023-02-10 23:37:50 +0100

    update sqlalchemy profiling

--- arbeitszeit_flask/profiling.py
+++ arbeitszeit_flask/profiling.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
-from flask import Flask, Response
-from flask_sqlalchemy import get_debug_queries
+from flask import Flask, Response, request
+from flask_sqlalchemy.record_queries import _QueryInfo, get_recorded_queries
 from werkzeug.middleware.profiler import ProfilerMiddleware
 
 
@@ -12,29 +12,31 @@ def show_profile_info(app):
 def show_sql_queries(app):
     @app.after_request
     def after_request(response: Response) -> Response:
-        queries: Dict[int, int] = dict()
-        for query in get_debug_queries():
+        queries: Dict[str, int] = dict()
+        for query in get_recorded_queries():
             queries = _update_queries_dict(queries, query)
 
-        _print_queries(queries)
+        if queries:
+            _print_queries(queries)
         return response
 
 
-def _update_queries_dict(queries, query):
-    if queries.get(query.context) is not None:
-        queries[query.context] += 1
+def _update_queries_dict(queries: Dict[str, int], query: _QueryInfo):
+    if queries.get(query.location) is not None:
+        queries[query.location] += 1
     else:
-        queries[query.context] = 1
+        queries[query.location] = 1
     return queries
 
 
 def _print_queries(queries):
     print()
-    print("SQLAlCHEMY COUNT QUERIES:")
-    print("=========================")
-    print("QueryCount", " Context")
-    for context in sorted(queries, key=queries.__getitem__, reverse=True):
-        print("{:>10}  {:<0}".format(queries[context], context))
+    print("SqlAlchemy queries")
+    print("===================")
+    print(f"Route: {request.path}. Method: {request.method}")
+    print("QueryCount", " Location")
+    for location in sorted(queries, key=queries.__getitem__, reverse=True):
+        print("{:>10}  {:<0}".format(queries[location], location))
     print()
 
 
