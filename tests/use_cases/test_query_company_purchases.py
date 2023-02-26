01d4aeac69a4023e6fb975380039331c96ec4fce
sebas <sebastian.loschert@gmx.de>
2023-02-26 20:21:41 +0100

    make test_query_company_purchases class based

--- tests/use_cases/test_query_company_purchases.py
+++ tests/use_cases/test_query_company_purchases.py
@@ -5,59 +5,45 @@ from arbeitszeit.use_cases.query_company_purchases import (
     PurchaseQueryResponse,
     QueryCompanyPurchases,
 )
-from tests.data_generators import CompanyGenerator, PurchaseGenerator
-from tests.datetime_service import FakeDatetimeService
-
-from .dependency_injection import injection_test
+from tests.use_cases.base_test_case import BaseTestCase
 
 
-def purchase_in_results(
-    purchase: Purchase, results: Iterable[PurchaseQueryResponse]
-) -> bool:
-    return purchase.plan in [result.plan_id for result in results]
+class UseCaseTests(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.query_purchases = self.injector.get(QueryCompanyPurchases)
 
+    def purchase_in_results(
+        self, purchase: Purchase, results: Iterable[PurchaseQueryResponse]
+    ) -> bool:
+        return purchase.plan in [result.plan_id for result in results]
 
-@injection_test
-def test_that_no_purchase_is_returned_when_searching_an_empty_repo(
-    query_purchases: QueryCompanyPurchases,
-    company_generator: CompanyGenerator,
-):
-    company = company_generator.create_company()
-    results = list(query_purchases(company))
-    assert not results
+    def test_that_no_purchase_is_returned_when_searching_an_empty_repo(
+        self,
+    ):
+        company = self.company_generator.create_company()
+        results = list(self.query_purchases(company))
+        assert not results
 
+    def test_that_correct_purchases_are_returned(self):
+        company = self.company_generator.create_company_entity()
+        expected_purchase_company = self.purchase_generator.create_purchase_by_company(
+            buyer=company
+        )
+        results = list(self.query_purchases(company.id))
+        assert len(results) == 1
+        assert self.purchase_in_results(expected_purchase_company, results)
 
-@injection_test
-def test_that_correct_purchases_are_returned(
-    query_purchases: QueryCompanyPurchases,
-    company_generator: CompanyGenerator,
-    purchase_generator: PurchaseGenerator,
-):
-    company = company_generator.create_company_entity()
-    expected_purchase_company = purchase_generator.create_purchase_by_company(
-        buyer=company
-    )
-    results = list(query_purchases(company.id))
-    assert len(results) == 1
-    assert purchase_in_results(expected_purchase_company, results)
-
-
-@injection_test
-def test_that_purchases_are_returned_in_correct_order(
-    query_purchases: QueryCompanyPurchases,
-    company_generator: CompanyGenerator,
-    purchase_generator: PurchaseGenerator,
-    datetime_service: FakeDatetimeService,
-):
-    company = company_generator.create_company_entity()
-    # Creating older purchase first to test correct ordering
-    purchase_generator.create_purchase_by_company(
-        buyer=company, purchase_date=datetime_service.now_minus_two_days()
-    )
-    expected_recent_purchase = purchase_generator.create_purchase_by_company(
-        buyer=company, purchase_date=datetime_service.now_minus_one_day()
-    )
-    results = list(query_purchases(company.id))
-    assert purchase_in_results(
-        expected_recent_purchase, [results[0]]
-    )  # more recent purchase is first
+    def test_that_purchases_are_returned_in_correct_order(self):
+        company = self.company_generator.create_company_entity()
+        # Creating older purchase first to test correct ordering
+        self.purchase_generator.create_purchase_by_company(
+            buyer=company, purchase_date=self.datetime_service.now_minus_two_days()
+        )
+        expected_recent_purchase = self.purchase_generator.create_purchase_by_company(
+            buyer=company, purchase_date=self.datetime_service.now_minus_one_day()
+        )
+        results = list(self.query_purchases(company.id))
+        assert self.purchase_in_results(
+            expected_recent_purchase, [results[0]]
+        )  # more recent purchase is first
