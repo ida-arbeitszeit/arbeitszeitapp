ab20ca93b995f219a7b6ccdc4d8387a7136df0b6
sebas <sebastian.loschert@gmx.de>
2023-03-10 13:03:51 +0100

    new const DEFAULT_PAGE_SIZE

--- arbeitszeit_web/controllers/query_companies_controller.py
+++ arbeitszeit_web/controllers/query_companies_controller.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Optional, Protocol
 
 from arbeitszeit.use_cases.query_companies import CompanyFilter, QueryCompaniesRequest
-from arbeitszeit_web.pagination import PAGE_PARAMETER_NAME
+from arbeitszeit_web.pagination import DEFAULT_PAGE_SIZE, PAGE_PARAMETER_NAME
 from arbeitszeit_web.request import Request
 
 
@@ -14,7 +14,7 @@ class QueryCompaniesFormData(Protocol):
         ...
 
 
-_PAGE_SIZE = 15
+_page_size = DEFAULT_PAGE_SIZE
 
 
 @dataclass
@@ -57,7 +57,7 @@ class QueryCompaniesController:
             query=query,
             filter_category=filter_category,
             offset=offset,
-            limit=_PAGE_SIZE,
+            limit=_page_size,
         )
 
     def _get_pagination_offset(self, request: Request) -> int:
@@ -68,4 +68,4 @@ class QueryCompaniesController:
             page_number = int(page_str)
         except ValueError:
             return 0
-        return (page_number - 1) * _PAGE_SIZE
+        return (page_number - 1) * _page_size
