eacd428d0617ca3821483c4f3dc9ae4576cba5be
sebas <sebastian.loschert@gmx.de>
2023-03-11 23:03:12 +0100

    add QueryCompaniesApiPresenter

--- arbeitszeit_flask/api/companies.py
+++ arbeitszeit_flask/api/companies.py
@@ -5,6 +5,7 @@ from arbeitszeit.use_cases.query_companies import (
     QueryCompanies as QueryCompaniesUseCase,
 )
 from arbeitszeit_flask.api.input_documentation import generate_input_documentation
+from arbeitszeit_flask.api.schema_converter import json_schema_to_flaskx
 from arbeitszeit_flask.dependency_injection import with_injection
 from arbeitszeit_flask.flask_request import FlaskRequest
 from arbeitszeit_web.api_controllers.errors import (
@@ -14,6 +15,9 @@ from arbeitszeit_web.api_controllers.errors import (
 from arbeitszeit_web.api_controllers.query_companies_api_controller import (
     QueryCompaniesApiController,
 )
+from arbeitszeit_web.api_presenters.query_companies_api_presenter import (
+    QueryCompaniesApiPresenter,
+)
 
 namespace = Namespace("companies", "Companies related endpoints.")
 
@@ -21,15 +25,21 @@ input_documentation = generate_input_documentation(
     QueryCompaniesApiController().create_expected_inputs()
 )
 
+model = json_schema_to_flaskx(
+    schema=QueryCompaniesApiPresenter().get_schema(), namespace=namespace
+)
+
 
 @namespace.route("")
 class QueryCompanies(Resource):
     @namespace.expect(input_documentation)
+    @namespace.marshal_with(model)
     @with_injection()
     def get(
         self,
         controller: QueryCompaniesApiController,
         query_companies: QueryCompaniesUseCase,
+        presenter: QueryCompaniesApiPresenter,
     ):
         """Query companies."""
         try:
@@ -39,5 +49,5 @@ class QueryCompanies(Resource):
         except NegativeNumberError:
             abort(400, "The parameter must not be be a negative number.")
         response = query_companies(use_case_request)
-        response
-        return "placeholder"
+        view_model = presenter.create_view_model(response)
+        return view_model
---
+++ tests/api/presenters/test_query_companies_presenter.py
@@ -0,0 +1,138 @@
+from arbeitszeit_web.api_presenters.interfaces import JsonDict, JsonInteger, JsonString
+from arbeitszeit_web.api_presenters.query_companies_api_presenter import (
+    QueryCompaniesApiPresenter,
+)
+from tests.api.presenters.base_test_case import BaseTestCase
+from tests.presenters.data_generators import QueriedCompanyGenerator
+
+
+class TestViewModelCreation(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.presenter = self.injector.get(QueryCompaniesApiPresenter)
+        self.queried_company_generator = self.injector.get(QueriedCompanyGenerator)
+
+    def test_presenter_shows_limit_as_None_when_no_limit_was_requested(self):
+        use_case_response = self.queried_company_generator.get_response(
+            requested_limit=None, requested_offset=10
+        )
+        view_model = self.presenter.create_view_model(use_case_response)
+        self.assertIsNone(view_model.limit)
+
+    def test_presenter_shows_offset_as_None_when_no_offset_was_requested(self):
+        use_case_response = self.queried_company_generator.get_response(
+            requested_offset=None, requested_limit=10
+        )
+        view_model = self.presenter.create_view_model(use_case_response)
+        self.assertIsNone(view_model.offset)
+
+    def test_view_model_shows_requested_offset(self):
+        use_case_response = self.queried_company_generator.get_response(
+            queried_companies=[], requested_offset=5, requested_limit=10
+        )
+        view_model = self.presenter.create_view_model(use_case_response)
+        self.assertEqual(view_model.offset, 5)
+
+    def test_view_model_shows_requested_limit(self):
+        use_case_response = self.queried_company_generator.get_response(
+            queried_companies=[], requested_offset=5, requested_limit=10
+        )
+        view_model = self.presenter.create_view_model(use_case_response)
+        self.assertEqual(view_model.limit, 10)
+
+    def test_view_model_shows_same_total_results_as_use_case(self):
+        use_case_response = self.queried_company_generator.get_response(
+            total_results=12, requested_offset=5, requested_limit=10
+        )
+        view_model = self.presenter.create_view_model(use_case_response)
+        self.assertEqual(view_model.total_results, 12)
+
+    def test_view_model_has_no_results_if_use_case_response_has_no_queried_company(
+        self,
+    ):
+        use_case_response = self.queried_company_generator.get_response(
+            queried_companies=[], requested_offset=0, requested_limit=10
+        )
+        view_model = self.presenter.create_view_model(use_case_response)
+        self.assertEqual(len(view_model.results), 0)
+
+    def test_view_model_has_one_result_if_use_case_response_has_one_queried_company(
+        self,
+    ):
+        company = self.queried_company_generator.get_company()
+        use_case_response = self.queried_company_generator.get_response(
+            queried_companies=[company], requested_offset=0, requested_limit=10
+        )
+        view_model = self.presenter.create_view_model(use_case_response)
+        self.assertEqual(len(view_model.results), 1)
+
+
+class TestSchema(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.queried_company_generator = self.injector.get(QueriedCompanyGenerator)
+        self.presenter = self.injector.get(QueryCompaniesApiPresenter)
+
+    def test_schema_top_level(self) -> None:
+        schema = self.presenter.get_schema()
+        assert isinstance(schema, JsonDict)
+        assert not schema.as_list
+        assert schema.schema_name == "CompanyList"
+
+    def test_schema_top_level_members(self) -> None:
+        schema = self.presenter.get_schema()
+        assert isinstance(schema, JsonDict)
+        assert isinstance(schema.members["results"], JsonDict)
+
+    def test_schema_top_level_members_field_types_are_correct(self) -> None:
+        top_level_schema = self.presenter.get_schema()
+        assert isinstance(top_level_schema, JsonDict)
+
+        field_expectations = [
+            ("results", JsonDict),
+            ("total_results", JsonInteger),
+            ("offset", JsonInteger),
+            ("limit", JsonInteger),
+        ]
+
+        assert len(top_level_schema.members) == len(field_expectations)
+
+        for field_name, expected_type in field_expectations:
+            assert isinstance(top_level_schema.members[field_name], expected_type)
+
+    def test_results_is_dictionary(self) -> None:
+        schema = self.presenter.get_schema()
+        assert isinstance(schema, JsonDict)
+        results_schema = schema.members["results"]
+        assert isinstance(results_schema, JsonDict)
+
+    def test_results_is_dictionary_as_list(self) -> None:
+        schema = self.presenter.get_schema()
+        assert isinstance(schema, JsonDict)
+        results_schema = schema.members["results"]
+        assert isinstance(results_schema, JsonDict)
+        assert results_schema.as_list
+
+    def test_results_has_correct_schema_name(self) -> None:
+        schema = self.presenter.get_schema()
+        assert isinstance(schema, JsonDict)
+        results_schema = schema.members["results"]
+        assert isinstance(results_schema, JsonDict)
+        assert results_schema.schema_name == "Company"
+
+    def test_results_members_field_types_are_correct(self) -> None:
+        schema = self.presenter.get_schema()
+        assert isinstance(schema, JsonDict)
+        results_schema = schema.members["results"]
+        assert isinstance(results_schema, JsonDict)
+
+        field_expectations = [
+            ("company_id", JsonString),
+            ("company_email", JsonString),
+            ("company_name", JsonString),
+        ]
+
+        assert len(results_schema.members) == len(field_expectations)
+
+        for field_name, expected_type in field_expectations:
+            assert isinstance(results_schema.members[field_name], expected_type)
