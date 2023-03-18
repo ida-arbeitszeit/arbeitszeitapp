6ba1cd539a1cf1caa3285eed65194b2ee8e71789
sebas <sebastian.loschert@gmx.de>
2023-03-18 20:26:59 +0100

    delete unnecessary api integration tests

--- tests/api/integration/test_list_active_plans_api.py
+++ tests/api/integration/test_list_active_plans_api.py
@@ -27,18 +27,3 @@ class ListActivePlansTests(ApiTestCase):
         response = self.client.post(self.url)
         self.assertEqual(response.mimetype, "application/json")
         self.assertEqual(response.status_code, 405)
-
-    def test_put_returns_405(self):
-        response = self.client.put(self.url)
-        self.assertEqual(response.mimetype, "application/json")
-        self.assertEqual(response.status_code, 405)
-
-    def test_patch_returns_405(self):
-        response = self.client.patch(self.url)
-        self.assertEqual(response.mimetype, "application/json")
-        self.assertEqual(response.status_code, 405)
-
-    def test_delete_returns_405(self):
-        response = self.client.delete(self.url)
-        self.assertEqual(response.mimetype, "application/json")
-        self.assertEqual(response.status_code, 405)
