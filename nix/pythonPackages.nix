251e0176e0a65c18fe61f3d4a42c8779143ba96f
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-05-02 16:08:04 +0200

    Remove nix packaging instructions for custom version of flask-restx
    This change removes the custom packaging instructions for the
    flask-restx package. Since it is packaged inside of nixpkgs there is
    no need to rely on a custom version. Instead we use the default
    version of flask-restx that comes with nixpkgs to reduce maintenance
    costs.

--- nix/pythonPackages.nix
+++ nix/pythonPackages.nix
@@ -1,5 +1,4 @@
 self: super: {
   arbeitszeitapp = self.callPackage pythonPackages/arbeitszeitapp.nix { };
   is_safe_url = self.callPackage pythonPackages/is_safe_url.nix { };
-  flask-restx = self.callPackage pythonPackages/flask-restx.nix { };
 }
