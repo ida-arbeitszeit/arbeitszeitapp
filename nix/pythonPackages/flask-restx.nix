3d7d68ed334f0aef00457c13a4435f1a3a43190c
sebas <sebastian.loschert@gmx.de>
2023-05-07 20:51:55 +0200

    update restx nix pkg manually to flask-restx=1.1.0

--- nix/pythonPackages/flask-restx.nix
+++ nix/pythonPackages/flask-restx.nix
@@ -3,12 +3,12 @@
 
 buildPythonPackage rec {
   pname = "flask-restx";
-  version = "1.0.5";
+  version = "1.1.0";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-4j3E/ySGnJL6pxm3pYvhID7XQSdf8yyfA9CrVu0BVGw=";
+    sha256 = "sha256-Yra2yd5l5ZYM9PizXhvT7KaZiDigGy9x4qnUwUpMzRQ=";
   };
 
   propagatedBuildInputs = [ flask aniso8601 jsonschema pytz werkzeug ];
