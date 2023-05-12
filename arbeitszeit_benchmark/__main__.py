14c3af285b11b6ec1cfbd1e07ea99f97db3637cf
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-05-12 08:35:00 +0200

    Implement rudimentary benchmark framework
    With this changeset we start providing a basic framework to measure
    the runtime characteristics of our code. Those benchmarks can be found
    under `/arbeitszeit_benchmark` and contain one example for now.

---
+++ arbeitszeit_benchmark/__main__.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import timeit
+from typing import Dict
+
+from .get_company_transactions import GetCompanyTransactionsBenchmark
+from .runner import BenchmarkCatalog, BenchmarkResult, render_results_as_json
+
+
+def main() -> None:
+    repeats = 5
+    results: Dict[str, BenchmarkResult] = dict()
+    catalog = BenchmarkCatalog()
+    catalog.register_benchmark(
+        name="get_company_transactions", benchmark_class=GetCompanyTransactionsBenchmark
+    )
+    for name, benchmark in catalog.get_all_benchmarks():
+        try:
+            average_benchmark_time = (
+                timeit.timeit(benchmark.run, number=repeats) / repeats
+            )
+        finally:
+            benchmark.tear_down()
+        results[name] = BenchmarkResult(
+            name=name, average_execution_time_in_secs=average_benchmark_time
+        )
+    print(render_results_as_json(results))
+
+
+main()
---
+++ arbeitszeit_benchmark/runner.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import json
+from dataclasses import dataclass
+from typing import Dict, Iterable, Protocol, Tuple, Type
+
+
+class BenchmarkCatalog:
+    def __init__(self) -> None:
+        self.registered_benchmark_classes: Dict[str, Type[Benchmark]] = dict()
+
+    def register_benchmark(self, name: str, benchmark_class: Type[Benchmark]) -> None:
+        self.registered_benchmark_classes[name] = benchmark_class
+
+    def get_all_benchmarks(self) -> Iterable[Tuple[str, Benchmark]]:
+        for name, benchmark_class in self.registered_benchmark_classes.items():
+            yield name, benchmark_class()
+
+
+class Benchmark(Protocol):
+    def __init__(self) -> None:
+        ...
+
+    def tear_down(self) -> None:
+        ...
+
+    def run(self) -> None:
+        ...
+
+
+@dataclass
+class BenchmarkResult:
+    name: str
+    average_execution_time_in_secs: float
+
+
+def render_results_as_json(results: Dict[str, BenchmarkResult]) -> str:
+    def result_to_json(result: BenchmarkResult):
+        return {
+            "name": result.name,
+            "average_execution_time_in_secs": result.average_execution_time_in_secs,
+        }
+
+    report_json = {name: result_to_json(result) for name, result in results.items()}
+    return json.dumps(report_json, indent=4)
