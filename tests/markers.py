b047cbf61a1e3813d625e8917ebb939e6543db30
sebas <sebastian.loschert@gmx.de>
2023-05-19 23:24:49 +0200

    tests.markers: fix problem with type-checking

--- tests/markers.py
+++ tests/markers.py
@@ -1,11 +1,12 @@
 import enum
 import os
-from typing import Set, Type
-from unittest import TestCase
+from typing import Set, TypeVar
 
 _MARKERS: Set[str] = {
     marker.strip() for marker in os.getenv("DISABLED_TESTS", "").split(",")
 }
+
+T = TypeVar("T")
 
 
 class Category(enum.Enum):
@@ -16,7 +17,7 @@ def _are_test_categories_enabled(*categories: Category) -> bool:
     return all(category.value not in _MARKERS for category in categories)
 
 
-def database_required(cls: Type[TestCase]) -> Type[TestCase]:
+def database_required(cls: type[T]) -> type[T]:
     class DatabaseTests(cls):  # type: ignore
         def setUp(self) -> None:
             if not _are_test_categories_enabled(Category.database_required):
