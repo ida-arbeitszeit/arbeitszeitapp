76b3a84f9b1d12b3ea840476c7faf77bbc9e4729
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-06-21 18:44:25 +0200

    fix bug in flask-restx nix packaging directive
    We used to have the package name and the version of `flask-restx`
    hardcoded in our nix package directives. With this change the correct
    metadata is read from the json file `flask-restx.json` that also
    serves as the source definition for fetchPypi.

--- nix/pythonPackages/flask-restx.nix
+++ nix/pythonPackages/flask-restx.nix
@@ -1,15 +1,11 @@
 { lib, buildPythonPackage, fetchPypi, flask, aniso8601, jsonschema, pytz
 , werkzeug }:
-
-buildPythonPackage rec {
-  pname = "flask-restx";
-  version = "1.1.0";
+let pypiSource = (builtins.fromJSON (builtins.readFile ./flask-restx.json));
+in buildPythonPackage rec {
+  pname = pypiSource.pname;
+  version = pypiSource.version;
   format = "setuptools";
-
-  src = fetchPypi (builtins.fromJSON (builtins.readFile ./flask-restx.json));
-
+  src = fetchPypi pypiSource;
   propagatedBuildInputs = [ flask aniso8601 jsonschema pytz werkzeug ];
-
   doCheck = false;
-
 }
