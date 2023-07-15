05f96dfc8ee3edde7f79553208f3c55d848987b9
Amittai Aviram <amittai.aviram@gmail.com>
2023-07-15 14:24:32 -0400

    Added failing test for creating an e-mail address.

--- tests/flask_integration/database_gateway_impl/test_email_result.py
+++ tests/flask_integration/database_gateway_impl/test_email_result.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import pytest
 from sqlalchemy.exc import IntegrityError
+from .utility import Utility
 
 from ..flask import FlaskTestCase
 
@@ -13,4 +14,13 @@ class EmailResultTests(FlaskTestCase):
         with pytest.raises(IntegrityError):
             self.database_gateway.create_email_address(
                 address=address, confirmed_on=datetime.min
+            )
+
+    def test_cannot_create_same_email_address_twice_case_insensitive(self) -> None:
+        address = "test@test.test"
+        self.database_gateway.create_email_address(address=address, confirmed_on=None)
+        altered_address = Utility.mangle_case(address) 
+        with pytest.raises(IntegrityError):
+            self.database_gateway.create_email_address(
+                address=altered_address, confirmed_on=datetime.min
             )
