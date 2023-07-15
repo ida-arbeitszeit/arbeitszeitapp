4c867287114766ea7ed6983a37c85fc508b8cf8d
Amittai Aviram <amittai.aviram@gmail.com>
2023-07-15 13:51:26 -0400

    Added two failing tests plus one passing to document behavior.

--- tests/flask_integration/database_gateway_impl/test_company_result.py
+++ tests/flask_integration/database_gateway_impl/test_company_result.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from arbeitszeit.entities import Company
 from arbeitszeit_flask.database.repositories import AccountRepository
 from tests.data_generators import CompanyGenerator, MemberGenerator
-from utility import Utility
+from .utility import Utility
 
 from ..flask import FlaskTestCase
 
@@ -44,6 +44,21 @@ class RepositoryTester(FlaskTestCase):
             == expected_company
         )
 
+    def test_can_retrieve_a_company_by_its_email_case_insensitive(
+        self,
+    ) -> None:
+        expected_email = "expected@mail.com"
+        expected_company = self.company_generator.create_company_entity(
+            email=expected_email
+        )
+        altered_email = Utility.mangle_case(expected_email)
+        assert (
+            self.database_gateway.get_companies()
+            .with_email_address(altered_email)
+            .first()
+            == expected_company
+        )
+
     def test_that_random_email_returns_no_company(self) -> None:
         random_email = "xyz123@testmail.com"
         self.company_generator.create_company_entity(email="test_mail@testmail.com")
@@ -75,6 +90,16 @@ class RepositoryTester(FlaskTestCase):
         assert not companies.with_email_address(expected_email)
         self.company_generator.create_company_entity(email=expected_email)
         assert companies.with_email_address(expected_email)
+
+    def test_can_detect_if_company_with_email_is_already_present_case_insensitive(
+        self,
+    ) -> None:
+        expected_email = "rosa@cp.org"
+        companies = self.database_gateway.get_companies()
+        altered_email = Utility.mangle_case(expected_email)
+        assert not companies.with_email_address(altered_email)
+        self.company_generator.create_company_entity(email=expected_email)
+        assert companies.with_email_address(altered_email)
 
     def test_does_not_identify_random_id_with_company(self) -> None:
         company_id = uuid4()
@@ -136,6 +161,25 @@ class CreateCompanyTests(FlaskTestCase):
             registered_on=self.timestamp,
         )
         self.db.session.flush()
+
+    def test_repository_stores_email_address_correctly(self) -> None:
+        email = "test@test.test"
+        company = self.database_gateway.create_company(
+            email=email,
+            name="test name",
+            password_hash="testpassword",
+            means_account=self.means_account,
+            labour_account=self.labour_account,
+            resource_account=self.resource_account,
+            products_account=self.products_account,
+            registered_on=self.timestamp,
+        )
+        self.db.session.flush()
+        retrieved_company = \
+                self.database_gateway.get_companies().with_id(company.id).first()
+        self.db.session.flush()
+        assert retrieved_company.email == email
+
 
 
 class ConfirmCompanyTests(FlaskTestCase):
@@ -257,13 +301,26 @@ class WithEmailContainingTests(FlaskTestCase):
         expected_company_id = self.company_generator.create_company(
             email="some.mail@cp.org"
         )
-        returned_company = list(
+        returned_companies = list(
             self.database_gateway.get_companies().with_email_containing(
                 "some.mail@cp.org"
             )
         )
-        assert returned_company
-        assert returned_company[0].id == expected_company_id
+        assert returned_companies
+        assert returned_companies[0].id == expected_company_id
+
+    def test_that_companies_can_be_filtered_by_email_case_insensitive(self):
+        email = "some.mail@cp.org"
+        expected_company_id = self.company_generator.create_company(
+            email=email
+        )
+        altered_email = Utility.mangle_case(email)
+        returned_companies = list(
+            self.database_gateway.get_companies().with_email_containing(
+                altered_email
+            )
+        )
+        assert returned_companies[0].id == expected_company_id
 
     def test_that_companies_can_be_filtered_by_substring_of_email(self):
         expected_company_id = self.company_generator.create_company(
@@ -301,3 +358,4 @@ class JoinedWithEmailTests(FlaskTestCase):
             .first()
         )
         assert record
+
