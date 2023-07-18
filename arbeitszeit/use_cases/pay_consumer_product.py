1194e2e4914d70007faec2ea94f58e827f0828d5
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-07-18 12:21:59 +0200

    Remove CannotSendMail and PlanIsInactive exception types
    The two exceptions that were removed in this change were largly
    obsolete. With some minor adjustment to the PayConsumerPurchase use
    case the code functions exactly as it did before.

--- arbeitszeit/use_cases/pay_consumer_product.py
+++ arbeitszeit/use_cases/pay_consumer_product.py
@@ -6,15 +6,14 @@ from enum import Enum, auto
 from typing import Optional
 from uuid import UUID
 
-from arbeitszeit import errors
 from arbeitszeit.datetime_service import DatetimeService
 from arbeitszeit.entities import Member, Plan
-from arbeitszeit.giro_office import GiroOffice, TransactionRejection
+from arbeitszeit.giro_office import GiroOffice, Transaction, TransactionRejection
 from arbeitszeit.price_calculator import PriceCalculator
 from arbeitszeit.repositories import DatabaseGateway
 
 
-class RejectionReason(Enum):
+class RejectionReason(Exception, Enum):
     plan_inactive = auto()
     plan_not_found = auto()
     insufficient_balance = auto()
@@ -49,13 +48,11 @@ class PayConsumerProduct:
     ) -> PayConsumerProductResponse:
         try:
             return self._perform_buying_process(request)
-        except errors.PlanIsInactive:
+        except RejectionReason as reason:
+            return PayConsumerProductResponse(rejection_reason=reason)
+        except TransactionRejection:
             return PayConsumerProductResponse(
-                rejection_reason=RejectionReason.plan_inactive
-            )
-        except self.PlanNotFound:
-            return PayConsumerProductResponse(
-                rejection_reason=RejectionReason.plan_not_found
+                rejection_reason=RejectionReason.insufficient_balance
             )
 
     def _perform_buying_process(
@@ -71,33 +68,28 @@ class PayConsumerProduct:
         individual_price_per_unit = self.price_calculator.calculate_individual_price(
             plan
         )
-        try:
-            self._transfer_certificates(
-                request.amount,
-                plan,
-                buyer,
-                coop_price_per_unit=coop_price_per_unit,
-                individual_price_per_unit=individual_price_per_unit,
-            )
-        except TransactionRejection:
-            return PayConsumerProductResponse(
-                rejection_reason=RejectionReason.insufficient_balance
-            )
+        transaction = self._transfer_certificates(
+            request.amount,
+            plan,
+            buyer,
+            coop_price_per_unit=coop_price_per_unit,
+            individual_price_per_unit=individual_price_per_unit,
+        )
+        self.database_gateway.create_consumer_purchase(
+            amount=request.amount,
+            plan=plan.id,
+            transaction=transaction.id,
+        )
         return PayConsumerProductResponse(rejection_reason=None)
 
     def _get_active_plan(self, request: PayConsumerProductRequest) -> Plan:
         now = self.datetime_service.now()
         plan = self.database_gateway.get_plans().with_id(request.plan).first()
         if plan is None:
-            raise self.PlanNotFound("Plan could not be found")
+            raise RejectionReason.plan_not_found
         if not plan.is_active_as_of(now):
-            raise errors.PlanIsInactive(
-                plan=plan,
-            )
+            raise RejectionReason.plan_inactive
         return plan
-
-    class PlanNotFound(ValueError):
-        pass
 
     def _transfer_certificates(
         self,
@@ -106,18 +98,13 @@ class PayConsumerProduct:
         buyer: Member,
         coop_price_per_unit: Decimal,
         individual_price_per_unit: Decimal,
-    ) -> None:
+    ) -> Transaction:
         planner = self.database_gateway.get_companies().with_id(plan.planner).first()
         assert planner
-        transaction = self.giro_office.record_transaction_from_member(
+        return self.giro_office.record_transaction_from_member(
             sender=buyer,
             receiving_account=planner.product_account,
             amount_sent=coop_price_per_unit * amount,
             amount_received=individual_price_per_unit * amount,
             purpose=f"Plan-Id: {plan.id}",
-        )
-        self.database_gateway.create_consumer_purchase(
-            amount=amount,
-            plan=plan.id,
-            transaction=transaction.id,
         )
