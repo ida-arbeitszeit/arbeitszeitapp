7d55896aaae9e9630fd80e15d7ff85d499e4e845
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-08-13 12:26:59 +0200

    Introduce check for missing migrations in run-checks

--- arbeitszeit_flask/configuration_base.py
+++ arbeitszeit_flask/configuration_base.py
@@ -4,7 +4,9 @@ FLASK_DEBUG = 0
 DEBUG_DETAILS = False
 TESTING = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/arbeitszeitapp.db"
+SQLALCHEMY_DATABASE_URI = environ.get(
+    "ARBEITSZEITAPP_DATABASE_URI", "sqlite:////tmp/arbeitszeitapp.db"
+)
 SECURITY_PASSWORD_SALT = environ.get("SECURITY_PASSWORD_SALT")
 LANGUAGES = {"en": "English", "de": "Deutsch"}
 MAIL_PORT = "25"
