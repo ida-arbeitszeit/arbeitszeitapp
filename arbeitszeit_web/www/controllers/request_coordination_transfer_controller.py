928096ec13f9c74d47bae0e1887a8daff32553e3
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-11-28 21:00:23 +0100

    Implement basic framework for parsing www form fields
    Before this change there was no guidance nor a pattern one could
    follow to implement www form field parsing. With this change a basic
    framework for form field parsing is introduced.
    
    The new functionality is located under `arbeitszeit_web.fields`. There
    is now a function `parse_formfield` that takes a `FormField` and a
    `FieldParser[T]` to produce an `Optional[T]` while attaching potential
    error messages to the form field. A `FieldParser` can be any
    `Callable` that receives a single string argument and produces
    `ParsingSuccess[T] | ParsingFailure`.
    
    The newly introduced framework was used to rework some www controllers
    which already performed form field parsing.
    
    One of the designs that were considered was an exception based control
    flow a la django with its
    [ValidationError](https://docs.djangoproject.com/en/4.2/ref/forms/fields/#django.forms.Field.clean).
    This approach was rejected as it used exceptions as a control flow
    mechanism which is only slightly less footgun-y than GOTO. Exceptions
    as control flow also make it slightly awkward to handle cases where
    multiple error messages should be attached in a single parsing process
    as one would have to aggregate the individual messages and the raise a
    single exception based on those. This is counter intuitive for some
    developers as usually exceptions are raise immediately if a
    calculation/function cannot be continued. The approach take where the
    parser yields a result of one of two types (`ParsingResult[T]` or
    `ParsingFailure`) might look a little bit unsual at first glance but
    allows the reader to infer the specification of a form field parser by
    looking at its type signature. An exception based control flow would
    not allow that and neither would it allow a type checker to assist
    debugging.
    
    It was also considered to remove the `ParsingResult` type and let
    `parse_formfield` return the parsed value directly. This would
    complicate cases where `None` is a valid parsing result though and was
    therefor rejected.
    
    Currently a `FieldParser` is expected to be a callable. We considered
    changing the interface so that there must be a named method a la
    `parse_form_field(self, value: str, /) -> ParsingSuccess[T] |
    ParsingFailure`. This was rejected as utilizing the `__call__` procol
    allows users to pass in functions for parsing as well as instances of
    custom classes. The danger of accidentally passing a non-parser
    function into `parse_formfields` without the type checker noticing was
    considered very very low as such a function would need to yield a
    `ParsingSuccess[T] | ParsingFailure` to be accepted.

--- arbeitszeit_web/www/controllers/request_coordination_transfer_controller.py
+++ arbeitszeit_web/www/controllers/request_coordination_transfer_controller.py
@@ -1,41 +1,34 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import Optional
-from uuid import UUID
 
 from arbeitszeit.use_cases.request_coordination_transfer import (
     RequestCoordinationTransferUseCase as UseCase,
 )
+from arbeitszeit_web.fields import parse_formfield
 from arbeitszeit_web.forms import RequestCoordinationTransferForm
 from arbeitszeit_web.request import Request
 from arbeitszeit_web.session import Session
-from arbeitszeit_web.translator import Translator
+from arbeitszeit_web.www.formfield_parsers import UuidParser
 
 
 @dataclass
 class RequestCoordinationTransferController:
     session: Session
     request: Request
-    translator: Translator
+    uuid_parser: UuidParser
 
     def import_form_data(
         self, form: RequestCoordinationTransferForm
     ) -> Optional[UseCase.Request]:
-        try:
-            candidate = UUID(form.candidate_field().get_value())
-        except ValueError:
-            form.candidate_field().attach_error(self.translator.gettext("Invalid UUID"))
-            return None
-        try:
-            cooperation = UUID(form.cooperation_field().get_value())
-        except ValueError:
-            form.cooperation_field().attach_error(
-                self.translator.gettext("Invalid UUID")
-            )
-            return None
+        candidate = parse_formfield(form.candidate_field(), self.uuid_parser)
+        cooperation = parse_formfield(form.cooperation_field(), self.uuid_parser)
         current_user = self.session.get_current_user()
-        assert current_user
+        if not (candidate and cooperation and current_user):
+            return None
         return UseCase.Request(
             requester=current_user,
-            cooperation=cooperation,
-            candidate=candidate,
+            cooperation=cooperation.value,
+            candidate=candidate.value,
         )
