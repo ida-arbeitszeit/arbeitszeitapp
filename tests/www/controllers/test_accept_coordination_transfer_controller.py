2770479dcaa1e8c841fc2b04068167657e32ae3f
Sebastian Loschert <sebastian.loschert@gmx.de>
2023-12-02 18:01:59 +0100

    Introduce AcceptCoordinationTransferController

---
+++ tests/www/controllers/test_accept_coordination_transfer_controller.py
@@ -0,0 +1,25 @@
+from uuid import uuid4
+
+from arbeitszeit_web.www.controllers.accept_coordination_transfer_controller import (
+    AcceptCoordinationTransferController,
+)
+from tests.www.base_test_case import BaseTestCase
+
+
+class AcceptCoordinationTransferControllerTests(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.controller = self.injector.get(AcceptCoordinationTransferController)
+        self.company = self.company_generator.create_company()
+        self.session.login_company(company=self.company)
+
+    def test_transfer_request_id_gets_passed_to_use_case_request(self) -> None:
+        expected = uuid4()
+        uc_request = self.controller.create_use_case_request(transfer_request=expected)
+        self.assertEqual(expected, uc_request.transfer_request_id)
+
+    def test_current_logged_in_company_gets_passed_to_use_case_request_as_accepting_company(
+        self,
+    ) -> None:
+        uc_request = self.controller.create_use_case_request(transfer_request=uuid4())
+        self.assertEqual(self.company, uc_request.accepting_company)
