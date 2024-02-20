80e500c5c61eb090de5fcaf0a0840103560e950c
petsch0 <peter.scholl@sap.com>
2024-02-20 21:54:04 +0100

    add text drafts for many fields

--- arbeitszeit_flask/static/main.js
+++ arbeitszeit_flask/static/main.js
@@ -67,7 +67,11 @@ document.addEventListener('DOMContentLoaded', () => {
     (document.querySelectorAll('.dropdown') || []).forEach(($dropdown) => {
       $dropdown.addEventListener('click', function(event) {
         event.stopPropagation();
-        $dropdown.classList.toggle('is-active');
+        $isOpen = $dropdown.classList.contains('is-active')
+        closeAllElementsByClassName('.dropdown')
+        if (!$isOpen) {
+          openElement($dropdown);
+        }
       });
     });
 
