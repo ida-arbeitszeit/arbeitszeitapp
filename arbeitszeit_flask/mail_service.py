01e3f5ad2c0d48e3fd54645ea19cf3235dbe1c44
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2024-05-05 15:34:18 +0200

    Implement email service utilizing smtplib
    This commit introduces a new `EmailService` implementation that
    utilizes Python's built-in `smtplib`. The objective is to eventually
    replace the existing flask_mail based implementation.

--- arbeitszeit_flask/mail_service.py
+++ arbeitszeit_flask/mail_service.py
@@ -1,4 +1,9 @@
-from typing import List
+from contextlib import contextmanager
+from dataclasses import dataclass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from smtplib import SMTP_SSL
+from typing import Generator, List
 
 from flask import current_app, render_template, url_for
 from flask_mail import Message
@@ -16,10 +21,13 @@ class FlaskEmailConfiguration:
 
 
 def get_mail_service() -> MailService:
-    if current_app.config.get("MAIL_BACKEND") == "flask_mail":
-        return FlaskMailService()
-    else:
-        return DebugMailService()
+    match current_app.config.get("MAIL_BACKEND"):
+        case "flask_mail":
+            return FlaskMailService()
+        case "smtplib_ssl":
+            return SmtpMailService()
+        case _:
+            return DebugMailService()
 
 
 class DebugMailService:
@@ -53,3 +61,35 @@ class FlaskMailService:
     ) -> None:
         msg = Message(subject=subject, recipients=recipients, html=html, sender=sender)
         mail.send(msg)
+
+
+@dataclass
+class SmtpMailService:
+    def send_message(
+        self,
+        subject: str,
+        recipients: List[str],
+        html: str,
+        sender: str,
+    ) -> None:
+        with self.create_smtp_connection() as smtp:
+            message = MIMEMultipart()
+            message["Subject"] = subject
+            message["From"] = sender
+            message.attach(MIMEText(html, "html"))
+            for recipient in recipients:
+                message["To"] = recipient
+                smtp.send_message(message)
+
+    @classmethod
+    @contextmanager
+    def create_smtp_connection(cls) -> Generator[SMTP_SSL, None, None]:
+        server = current_app.config.get("MAIL_SERVER", "localhost")
+        port = current_app.config.get("MAIL_PORT", "0")
+        smtp = SMTP_SSL(server, port=port)
+        smtp.ehlo()
+        username = current_app.config.get("MAIL_USERNAME", "")
+        password = current_app.config.get("MAIL_PASSWORD", "")
+        smtp.login(username, password)
+        yield smtp
+        smtp.quit()
