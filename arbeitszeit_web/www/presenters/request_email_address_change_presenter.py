095273afb3c27e62866cbb0803cd2a30859b0219
Sebastian Loschert <sebastian.loschert@gmx.de>
2024-07-17 16:42:25 +0200

    Refactor `RequestEmailAddressChangePresenter`
    Replace if-else with match-case statement.

--- arbeitszeit_web/www/presenters/request_email_address_change_presenter.py
+++ arbeitszeit_web/www/presenters/request_email_address_change_presenter.py
@@ -24,47 +24,45 @@ class RequestEmailAddressChangePresenter:
     def render_response(
         self, uc_response: use_case.Response, form: RequestEmailAddressChangeForm
     ) -> ViewModel:
-        redirect_url: Optional[str]
-        if uc_response.is_rejected:
-            if (
-                uc_response.rejection_reason
-                == use_case.Response.RejectionReason.invalid_email_address
-            ):
+        match uc_response.rejection_reason:
+            case None:
+                self._notify_about_acceptance()
+                redirect_url = self.url_index.get_user_account_details_url()
+                return ViewModel(redirect_url=redirect_url)
+            case use_case.Response.RejectionReason.invalid_email_address:
                 form.new_email_field.attach_error(
                     self.translator.gettext("The email address seems to be invalid.")
                 )
-            elif (
-                uc_response.rejection_reason
-                == use_case.Response.RejectionReason.current_email_address_does_not_exist
-            ):
-                pass
-            elif (
-                uc_response.rejection_reason
-                == use_case.Response.RejectionReason.new_email_address_already_taken
-            ):
+                self._notify_about_rejection()
+                return ViewModel(redirect_url=None)
+            case use_case.Response.RejectionReason.current_email_address_does_not_exist:
+                self._notify_about_rejection()
+                return ViewModel(redirect_url=None)
+            case use_case.Response.RejectionReason.new_email_address_already_taken:
                 form.new_email_field.attach_error(
                     self.translator.gettext(
                         "The email address seems to be already taken."
                     )
                 )
-            elif (
-                uc_response.rejection_reason
-                == use_case.Response.RejectionReason.incorrect_password
-            ):
+                self._notify_about_rejection()
+                return ViewModel(redirect_url=None)
+            case use_case.Response.RejectionReason.incorrect_password:
                 form.current_password_field.attach_error(
                     self.translator.gettext("The password is incorrect.")
                 )
-            redirect_url = None
-            self.notifier.display_warning(
-                self.translator.gettext(
-                    "Your request for an email address change was rejected."
-                )
+                self._notify_about_rejection()
+                return ViewModel(redirect_url=None)
+
+    def _notify_about_acceptance(self) -> None:
+        self.notifier.display_info(
+            self.translator.gettext(
+                "A confirmation mail has been sent to your new email address."
             )
-        else:
-            self.notifier.display_info(
-                self.translator.gettext(
-                    "A confirmation mail has been sent to your new email address."
-                )
+        )
+
+    def _notify_about_rejection(self) -> None:
+        self.notifier.display_warning(
+            self.translator.gettext(
+                "Your request for an email address change was rejected."
             )
-            redirect_url = self.url_index.get_user_account_details_url()
-        return ViewModel(redirect_url=redirect_url)
+        )
