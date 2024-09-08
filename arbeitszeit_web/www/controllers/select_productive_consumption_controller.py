9c9828c625e95851e9cfa3a09d512b44ffb3122c
Sebastian Loschert <sebastian.loschert@gmx.de>
2024-09-08 13:38:45 +0200

    Implement selection of productive consumption
    This commit creates SelectProductiveConsumptionUseCase, -Presenter and -Controller and uses these in the RegisterProductiveConsumptionView. They are intended to handle the GET part of this view, leaving POST mostly untouched.
    
    It implements a user flow described in issue #1053. New features:
    
    * The user sees some plan info before they have the chance to register consumption.
    * The provided plan ID appears as a read-only form field if it is valid.
    * There are two new buttons ("Select" and "Cancel").

---
+++ arbeitszeit_web/www/controllers/select_productive_consumption_controller.py
@@ -0,0 +1,96 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from arbeitszeit.records import ConsumptionType
+from arbeitszeit.use_cases.select_productive_consumption import (
+    Request as UseCaseRequest,
+)
+from arbeitszeit_web.notification import Notifier
+from arbeitszeit_web.request import Request
+from arbeitszeit_web.translator import Translator
+
+
+@dataclass
+class SelectProductiveConsumptionController:
+    class InputDataError(Exception):
+        pass
+
+    request: Request
+    notifier: Notifier
+    translator: Translator
+
+    def process_input_data(self) -> UseCaseRequest:
+        plan_id = self._process_plan_id()
+        amount = self._process_amount()
+        consumption_type = self._process_consumption_type()
+        return UseCaseRequest(
+            plan_id=plan_id, amount=amount, consumption_type=consumption_type
+        )
+
+    def _process_plan_id(self) -> UUID | None:
+        plan_id_from_query_string = self.request.query_string().get("plan_id")
+        plan_id_from_form = self.request.get_form("plan_id")
+        if not plan_id_from_query_string and not plan_id_from_form:
+            return None
+        elif plan_id_from_query_string:
+            return self._convert_string_to_uuid(plan_id_from_query_string)
+        else:
+            assert plan_id_from_form
+            return self._convert_string_to_uuid(plan_id_from_form)
+
+    def _convert_string_to_uuid(self, input_string: str) -> UUID:
+        try:
+            return UUID(input_string)
+        except ValueError:
+            self.notifier.display_warning(
+                self.translator.gettext("The provided plan ID is not a valid UUID.")
+            )
+            raise self.InputDataError()
+
+    def _process_amount(self) -> int | None:
+        amount_from_query_string = self.request.query_string().get("amount")
+        amount_from_form = self.request.get_form("amount")
+        if not amount_from_query_string and not amount_from_form:
+            return None
+        elif amount_from_query_string:
+            return self._convert_string_to_int(amount_from_query_string)
+        else:
+            assert amount_from_form
+            return self._convert_string_to_int(amount_from_form)
+
+    def _convert_string_to_int(self, input_string: str) -> int:
+        try:
+            return int(input_string)
+        except ValueError:
+            self.notifier.display_warning(
+                self.translator.gettext("The provided amount is not a valid integer.")
+            )
+            raise self.InputDataError()
+
+    def _process_consumption_type(self) -> ConsumptionType | None:
+        consumption_type_from_query_string = self.request.query_string().get(
+            "type_of_consumption"
+        )
+        consumption_type_from_form = self.request.get_form("type_of_consumption")
+        if not consumption_type_from_query_string and not consumption_type_from_form:
+            return None
+        elif consumption_type_from_query_string:
+            return self._convert_string_to_consumption_type(
+                consumption_type_from_query_string
+            )
+        else:
+            assert consumption_type_from_form
+            return self._convert_string_to_consumption_type(consumption_type_from_form)
+
+    def _convert_string_to_consumption_type(self, input_string: str) -> ConsumptionType:
+        if input_string == "fixed":
+            return ConsumptionType.means_of_prod
+        elif input_string == "liquid":
+            return ConsumptionType.raw_materials
+        else:
+            self.notifier.display_warning(
+                self.translator.gettext(
+                    "The provided type of consumption is not valid."
+                )
+            )
+            raise self.InputDataError()
