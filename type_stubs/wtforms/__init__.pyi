5edfa64f4c4c813b6ac7f3f2ebcf9f0a3a92e2a4
Sebastian Loschert <sebastian.loschert@gmx.de>
2024-12-07 20:29:16 +0100

    Remove dependency flask-sqlalchemy
    Dependency definitions have been modified and changes have been applied by running `python -m arbeitszeit_development.update_dependencies` and `python format_code.py`

--- type_stubs/wtforms/__init__.pyi
+++ type_stubs/wtforms/__init__.pyi
@@ -1,7 +1,77 @@
 from wtforms import validators as validators
 from wtforms import widgets as widgets
-from wtforms.fields import *
+from wtforms.fields.choices import RadioField as RadioField
+from wtforms.fields.choices import SelectField as SelectField
+from wtforms.fields.choices import SelectFieldBase as SelectFieldBase
+from wtforms.fields.choices import SelectMultipleField as SelectMultipleField
+from wtforms.fields.core import Field as Field
+from wtforms.fields.core import Flags as Flags
+from wtforms.fields.core import Label as Label
+from wtforms.fields.datetime import DateField as DateField
+from wtforms.fields.datetime import DateTimeField as DateTimeField
+from wtforms.fields.datetime import DateTimeLocalField as DateTimeLocalField
+from wtforms.fields.datetime import MonthField as MonthField
+from wtforms.fields.datetime import TimeField as TimeField
+from wtforms.fields.datetime import WeekField as WeekField
+from wtforms.fields.form import FormField as FormField
+from wtforms.fields.list import FieldList as FieldList
+from wtforms.fields.numeric import DecimalField as DecimalField
+from wtforms.fields.numeric import DecimalRangeField as DecimalRangeField
+from wtforms.fields.numeric import FloatField as FloatField
+from wtforms.fields.numeric import IntegerField as IntegerField
+from wtforms.fields.numeric import IntegerRangeField as IntegerRangeField
+from wtforms.fields.simple import BooleanField as BooleanField
+from wtforms.fields.simple import ColorField as ColorField
+from wtforms.fields.simple import EmailField as EmailField
+from wtforms.fields.simple import FileField as FileField
+from wtforms.fields.simple import HiddenField as HiddenField
+from wtforms.fields.simple import MultipleFileField as MultipleFileField
+from wtforms.fields.simple import PasswordField as PasswordField
+from wtforms.fields.simple import SearchField as SearchField
+from wtforms.fields.simple import StringField as StringField
+from wtforms.fields.simple import SubmitField as SubmitField
+from wtforms.fields.simple import TelField as TelField
+from wtforms.fields.simple import TextAreaField as TextAreaField
+from wtforms.fields.simple import URLField as URLField
 from wtforms.form import Form as Form
 from wtforms.validators import ValidationError as ValidationError
 
-__version__: str
+__all__ = [
+    "validators",
+    "widgets",
+    "Form",
+    "ValidationError",
+    "SelectField",
+    "SelectMultipleField",
+    "SelectFieldBase",
+    "RadioField",
+    "Field",
+    "Flags",
+    "Label",
+    "DateTimeField",
+    "DateField",
+    "TimeField",
+    "MonthField",
+    "DateTimeLocalField",
+    "WeekField",
+    "FormField",
+    "FieldList",
+    "IntegerField",
+    "DecimalField",
+    "FloatField",
+    "IntegerRangeField",
+    "DecimalRangeField",
+    "BooleanField",
+    "TextAreaField",
+    "PasswordField",
+    "FileField",
+    "MultipleFileField",
+    "HiddenField",
+    "SearchField",
+    "SubmitField",
+    "StringField",
+    "TelField",
+    "URLField",
+    "EmailField",
+    "ColorField",
+]
--- type_stubs/wtforms/fields/__init__.pyi
+++ type_stubs/wtforms/fields/__init__.pyi
@@ -1,10 +1,71 @@
+from wtforms.fields.choices import RadioField as RadioField
+from wtforms.fields.choices import SelectField as SelectField
 from wtforms.fields.choices import SelectFieldBase as SelectFieldBase
-from wtforms.fields.choices import *
+from wtforms.fields.choices import SelectMultipleField as SelectMultipleField
 from wtforms.fields.core import Field as Field
 from wtforms.fields.core import Flags as Flags
 from wtforms.fields.core import Label as Label
-from wtforms.fields.datetime import *
-from wtforms.fields.form import *
-from wtforms.fields.list import *
-from wtforms.fields.numeric import *
-from wtforms.fields.simple import *
+from wtforms.fields.datetime import DateField as DateField
+from wtforms.fields.datetime import DateTimeField as DateTimeField
+from wtforms.fields.datetime import DateTimeLocalField as DateTimeLocalField
+from wtforms.fields.datetime import MonthField as MonthField
+from wtforms.fields.datetime import TimeField as TimeField
+from wtforms.fields.datetime import WeekField as WeekField
+from wtforms.fields.form import FormField as FormField
+from wtforms.fields.list import FieldList as FieldList
+from wtforms.fields.numeric import DecimalField as DecimalField
+from wtforms.fields.numeric import DecimalRangeField as DecimalRangeField
+from wtforms.fields.numeric import FloatField as FloatField
+from wtforms.fields.numeric import IntegerField as IntegerField
+from wtforms.fields.numeric import IntegerRangeField as IntegerRangeField
+from wtforms.fields.simple import BooleanField as BooleanField
+from wtforms.fields.simple import ColorField as ColorField
+from wtforms.fields.simple import EmailField as EmailField
+from wtforms.fields.simple import FileField as FileField
+from wtforms.fields.simple import HiddenField as HiddenField
+from wtforms.fields.simple import MultipleFileField as MultipleFileField
+from wtforms.fields.simple import PasswordField as PasswordField
+from wtforms.fields.simple import SearchField as SearchField
+from wtforms.fields.simple import StringField as StringField
+from wtforms.fields.simple import SubmitField as SubmitField
+from wtforms.fields.simple import TelField as TelField
+from wtforms.fields.simple import TextAreaField as TextAreaField
+from wtforms.fields.simple import URLField as URLField
+from wtforms.utils import unset_value as _unset_value
+
+__all__ = [
+    "Field",
+    "Flags",
+    "Label",
+    "SelectField",
+    "SelectMultipleField",
+    "SelectFieldBase",
+    "RadioField",
+    "DateTimeField",
+    "DateField",
+    "TimeField",
+    "MonthField",
+    "DateTimeLocalField",
+    "WeekField",
+    "FormField",
+    "IntegerField",
+    "DecimalField",
+    "FloatField",
+    "IntegerRangeField",
+    "DecimalRangeField",
+    "BooleanField",
+    "TextAreaField",
+    "PasswordField",
+    "FileField",
+    "MultipleFileField",
+    "HiddenField",
+    "SearchField",
+    "SubmitField",
+    "StringField",
+    "TelField",
+    "URLField",
+    "EmailField",
+    "ColorField",
+    "FieldList",
+    "_unset_value",
+]
