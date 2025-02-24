2e41cf2af248c16274d523bbe96705e9c66aa3f4
petsch0 <peter.scholl06@gmail.com>
2025-02-24 19:23:48 +0100

    use html inline onclick event

--- arbeitszeit_flask/static/main.js
+++ arbeitszeit_flask/static/main.js
@@ -1,79 +1,63 @@
 function expandMenu() {
-    var element = document.getElementById("navbarOnTop");
-    element.classList.toggle("is-active");
+  var element = document.getElementById("navbarOnTop");
+  element.classList.toggle("is-active");
+}
+
+function togglePasswordVisibility(clickedElement) {
+  let input = clickedElement.parentElement.firstElementChild
+  let eye = input.nextElementSibling.nextElementSibling
+  if (input.type === "password") {
+    input.type = "text"
+    eye.classList.add("is-hidden")
+    eye.nextElementSibling.classList.remove("is-hidden")
+  } else {
+    input.type = "password"
+    eye.classList.remove("is-hidden")
+    eye.nextElementSibling.classList.add("is-hidden")
+  }
 }
 
 // close notification box
 document.addEventListener('DOMContentLoaded', () => {
-    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-        const $notification = $delete.parentNode;
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
 
-        $delete.addEventListener('click', () => {
-            $notification.parentNode.removeChild($notification);
-        });
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
     });
+  });
 });
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close an element
-    function openElement($el) {
-      $el.classList.add('is-active');
-    }
+  // Functions to open and close an element
+  function openElement($el) {
+    $el.classList.add('is-active');
+  }
 
-    function closeElement($el) {
-      $el.classList.remove('is-active');
-    }
-  
-    function closeAllElementsByClassName($className) {
-      (document.querySelectorAll($className) || []).forEach(($elem) => {
-        closeElement($elem);
-      });
-    }
+  function closeElement($el) {
+    $el.classList.remove('is-active');
+  }
 
-    document.addEventListener('click', function() {
-      closeAllElementsByClassName('.dropdown');
+  function closeAllElementsByClassName($className) {
+    (document.querySelectorAll($className) || []).forEach(($elem) => {
+      closeElement($elem);
     });
+  }
 
-    (document.querySelectorAll('.dropdown') || []).forEach(($dropdown) => {
-      $dropdown.addEventListener('click', function(event) {
-        event.stopPropagation();
-        $isOpen = $dropdown.classList.contains('is-active')
-        closeAllElementsByClassName('.dropdown')
-        if (!$isOpen) {
-          openElement($dropdown);
-        }
-      });
-    });
-
+  document.addEventListener('click', function () {
+    closeAllElementsByClassName('.dropdown');
   });
 
-// toggle password visibility
-document.addEventListener('DOMContentLoaded', () => {
-  $eye = document.getElementById("eye");
-  $eyeSlashed = document.getElementById("eye-slashed");
-  $pwInput = document.getElementById("password");
-  $eyeRepeat = document.getElementById("eye-repeat");
-  $eyeSlashedRepeat = document.getElementById("eye-slashed-repeat");
-  $pwInputRepeated = document.getElementById("repeat_password");
-    $eye.addEventListener('click', () => {
-      $eye.classList.add("is-hidden")
-      $eyeSlashed.classList.remove("is-hidden")
-      $pwInput.type = 'text'
-    });
-    $eyeSlashed.addEventListener('click', () => {
-      $eyeSlashed.classList.add("is-hidden")
-      $eye.classList.remove("is-hidden")
-      $pwInput.type = 'password'
-    });
-    $eyeRepeat.addEventListener('click', () => {
-      $eyeRepeat.classList.add("is-hidden")
-      $eyeSlashedRepeat.classList.remove("is-hidden")
-      $pwInputRepeated.type = 'text'
-    });
-    $eyeSlashedRepeat.addEventListener('click', () => {
-      $eyeSlashedRepeat.classList.add("is-hidden")
-      $eyeRepeat.classList.remove("is-hidden")
-      $pwInputRepeated.type = 'password'
+  (document.querySelectorAll('.dropdown') || []).forEach(($dropdown) => {
+    $dropdown.addEventListener('click', function (event) {
+      event.stopPropagation();
+      let isOpen = $dropdown.classList.contains('is-active')
+      closeAllElementsByClassName('.dropdown')
+      if (!isOpen) {
+        openElement($dropdown);
+      }
     });
   });
+
+});
