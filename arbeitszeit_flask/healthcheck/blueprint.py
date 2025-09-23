7c7ac1b65a1d32d44a51717ee7a19ca5b906577e
petsch0 <peter.scholl06@gmail.com>
2025-09-23 21:33:54 +0200

    remove unnecessary comments

--- arbeitszeit_flask/healthcheck/blueprint.py
+++ arbeitszeit_flask/healthcheck/blueprint.py
@@ -3,10 +3,8 @@ from flask import Blueprint
 from arbeitszeit_flask.class_based_view import as_flask_view
 from arbeitszeit_flask.views.healthcheck_view import HealthcheckView
 
-# Create a blueprint for health checks that doesn't require authentication
 healthcheck_blueprint = Blueprint("healthcheck", __name__)
 
-# Register the health check endpoint
 healthcheck_blueprint.route("/health", methods=["GET"])(
     as_flask_view()(HealthcheckView)
 )
--- arbeitszeit_flask/views/healthcheck_view.py
+++ arbeitszeit_flask/views/healthcheck_view.py
@@ -5,10 +5,6 @@ import flask
 
 @dataclass
 class HealthcheckView:
-    """Simple endpoint that returns 200 OK to indicate the application is running.
-    Used by Docker healthchecks and deployment tests.
-    """
 
     def GET(self) -> flask.Response:
-        """Return 200 OK to indicate the application is running."""
         return flask.Response(status=200)
